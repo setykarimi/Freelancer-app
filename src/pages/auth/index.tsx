7e474a3a2f9a36f7e00e9fb48a4c55216029266a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-18 21:37:19 +0330

    Subject: Authentication, Description: Uncomment sendOtp, Date: 12/18/2023

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,10 +1,11 @@
 import CheckOTPForm from "../../features/authentication/check-otp-form";
+import SendOTPForm from "../../features/authentication/send-otp-form";
 
 export default function AuthPage() {
   return (
     <div className="flex justify-center pt-10">
       <div>
-        {/* <SendOTPForm /> */}
+        <SendOTPForm />
         <CheckOTPForm />
       </div>
     </div>
