e82e790d42190f04ccf2f0a5abaebc83707e0bf6
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-19 21:44:23 +0330

    Subject: Freelancer app, Description: Add toaster, Date: 12/19/2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Route, Routes } from "react-router-dom";
 import AuthPage from "./pages/auth";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster/>
       <div className="container">
         <Routes>
           <Route path="/auth" element={<AuthPage />} />
