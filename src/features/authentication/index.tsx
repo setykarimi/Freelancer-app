e9c616645454935962d470300d2e4b55accabda5
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-20 21:43:24 +0330

    Subject: Freelancer app, Description: Edit content, Date: 12/20/2023

--- src/features/authentication/index.tsx
+++ src/features/authentication/index.tsx
@@ -1,20 +1,62 @@
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
-import SendOTPForm from "./send-otp-form";
+import toast from "react-hot-toast";
+import { getOtp } from "../../services/auth-service";
 import CheckOTPForm from "./check-otp-form";
+import SendOTPForm from "./send-otp-form";
 
 export default function AuthContainer() {
   const [step, setStep] = useState(1);
   const [phoneNumber, setPhoneNumber] = useState("");
+  const {
+    isPending: isSendingOtp,
+    error,
+    data,
+    mutateAsync,
+  } = useMutation({
+    mutationFn: getOtp,
+  });
+
+  const sendOtpHandler = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const data = await mutateAsync({
+        phoneNumber,
+      });
+      console.log("data", data);
+      setStep(2);
+      toast.success(data?.message);
+    } catch (error: any) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
 
   const renderStep = () => {
     switch (step) {
       case 1:
-        return <SendOTPForm setStep={setStep} setPhoneNumber={setPhoneNumber} phoneNumber={phoneNumber}/>;
+        return (
+          <SendOTPForm
+            isSendingOtp={isSendingOtp}
+            onSubmit={sendOtpHandler}
+            setPhoneNumber={setPhoneNumber}
+            phoneNumber={phoneNumber}
+          />
+        );
       case 2:
-        return <CheckOTPForm phoneNumber={phoneNumber}/>;
+        return (
+          <CheckOTPForm
+            phoneNumber={phoneNumber}
+            onBack={() => setStep((prev) => prev - 1)}
+            onResendOtp={sendOtpHandler}
+          />
+        );
       default:
         return null;
     }
   };
-  return <div className="flex justify-center pt-10"><div>{renderStep()}</div></div>;
+  return (
+    <div className="flex justify-center pt-10">
+      <div>{renderStep()}</div>
+    </div>
+  );
 }
