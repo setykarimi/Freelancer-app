354f7eff73181292b70dadd833e4a9386949084f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-21 09:21:03 +0330

    Subject: Authorization, Description: Add new route for complete profile, Date: 12/21/2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 import AuthPage from "./pages/auth";
-import { Toaster } from "react-hot-toast";
+import CompleteProfile from "./pages/complete-profile";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <Toaster/>
+      <Toaster />
       <div className="container">
         <Routes>
           <Route path="/auth" element={<AuthPage />} />
+          <Route path="/complete-profile" element={<CompleteProfile />} />
         </Routes>
       </div>
     </QueryClientProvider>
