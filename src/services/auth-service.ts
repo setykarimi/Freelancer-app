25b1b27ea8f3368ef832c8e52e7a45b4ed65afb2
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-23 21:26:23 +0330

    Subject: Authentication, Description: Add new api calling, Date: 12/23/2023

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,9 +1,17 @@
 import http from "./http-service";
 
 export function getOtp(data:any) {
-  return http.post("/user/get-otp", data).then(({ data }) => data.data);
+  return http.post("/user/get-otp", data).then(({ data }) => data.data).catch((err)=>{
+    console.log(err);
+    
+    return err
+  });
 }
 
 export function checkOtp(data:any) {
   return http.post("/user/check-otp", data).then(({ data }) => data.data);
+}
+
+export function completeProfile(data:any) {
+  return http.post("/user/complete-profile", data).then(({ data }) => data.data);
 }
