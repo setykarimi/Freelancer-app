a8de0ab70f317e10a4aac3e572248941aaf8be80
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-25 20:26:26 +0330

    Subject: freelancer app, Description: Edit profile page, Date: 12/25/2023

--- src/pages/complete-profile/index.tsx
+++ src/pages/complete-profile/index.tsx
@@ -1,6 +1,9 @@
 import CompleteProfileForm from "../../features/authentication/complete-profile-form";
 
 export default function CompleteProfile() {
-
-  return <CompleteProfileForm />;
+  return (
+    <div className="container xl:max-w-screen-xl">
+      <CompleteProfileForm />
+    </div>
+  );
 }
