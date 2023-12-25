dfc742636a0952b3e01819c4f7e3dc5c0e81448c
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-25 20:27:09 +0330

    Subject: freelancer app, Description: Add new route, Date: 12/25/2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
+import Layout from "./common/layout";
 import AuthPage from "./pages/auth";
 import CompleteProfile from "./pages/complete-profile";
+import Owner from "./pages/dashboard/owner";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
-      <div className="container">
-        <Routes>
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/complete-profile" element={<CompleteProfile />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/complete-profile" element={<CompleteProfile />} />
+        <Route element={<Layout />}>
+          <Route path="/owner" element={<Owner />} />
+        </Route>
+      </Routes>
     </QueryClientProvider>
   );
 }
