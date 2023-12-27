927f093c974c5e5dc44a4e32cd69f2bba4dfe675
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-27 21:32:37 +0330

    Subject: freelancer app, Description: Edit style, Date: 12/27/2023

--- src/common/layout/components/header.tsx
+++ src/common/layout/components/header.tsx
@@ -2,5 +2,5 @@ import useUser from "../../../services/use-user";
 
 export default function Navbar() {
   const { data } = useUser();
-  return <div className="bg-secondary-0 py-4 px-8">App header</div>;
+  return <div className="bg-secondary-0 py-4 px-8 border-b border-b-secondary-100">App header</div>;
 }
