cda20dc25917fad15e584201a6fdc4ffc327e086
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-27 21:33:29 +0330

    Subject: freelancer app, Description: Edit style and content, Date: 12/27/2023

--- src/common/layout/components/sidebar.tsx
+++ src/common/layout/components/sidebar.tsx
@@ -1,5 +1,23 @@
+import { NavLink } from "react-router-dom";
+
 export default function Sidebar() {
   return (
-    <div className="bg-secondary-0 row-start-1 row-span-2">App sidebar</div>
+    <div className="bg-secondary-0 row-start-1 row-span-2 border-l border-l-secondary-200 p-4">
+      <ul>
+        <li>
+          <NavLink
+            to="/owner/dashboard"
+            className={({ isActive }) => {
+              isActive ? "bg-primary-100/50" : "";
+            }}
+          >
+            خانه
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/owner/dashboard">پروژه‌ها</NavLink>
+        </li>
+      </ul>
+    </div>
   );
 }
