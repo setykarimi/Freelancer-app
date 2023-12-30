1896ed85b0842d1eeed5d3ed5fa8711a82e58c9c
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-30 21:35:48 +0330

    Subject: TypeScript errors, Description: Edit typescript errors, Date: 12/30/2023

--- src/features/projects/table/components/project-row.tsx
+++ src/features/projects/table/components/project-row.tsx
@@ -1,11 +1,17 @@
 import { HiOutlineTrash } from "react-icons/hi";
-import { TbPenciMinus } from "react-icons/tb";
+import { TbPencilMinus } from "react-icons/tb";
 import Table from "../../../../common/table";
 import toLocalDateShort from "../../../../utils/to-local-date-short";
 import { toPersianNumbersWithComma } from "../../../../utils/to-persian-numbers";
 import truncateText from "../../../../utils/truncate-text";
 
-export default function ProjectRow({project, index}) {
+export default function ProjectRow({
+  project,
+  index,
+}: {
+  project: any;
+  index: number;
+}) {
   return (
     <>
       <Table.Row key={project.id}>
@@ -16,7 +22,7 @@ export default function ProjectRow({project, index}) {
         <td>{toLocalDateShort(project.deadline)}</td>
         <td>
           <div className="flex flex-wrap items-center gap-2 max-w-[200px]">
-            {project.tags.map((tag) => (
+            {project.tags.map((tag: string) => (
               <span className="badge badge--secondary" key={tag}>
                 {tag}
               </span>
@@ -32,8 +38,12 @@ export default function ProjectRow({project, index}) {
           )}
         </td>
         <td>
-          <button><TbPenciMinus className="w-5 h-5 text-primary-"/></button>
-          <button><HiOutlineTrash /></button>
+          <button>
+            <TbPencilMinus className="w-5 h-5 text-primary-" />
+          </button>
+          <button>
+            <HiOutlineTrash />
+          </button>
         </td>
       </Table.Row>
     </>
