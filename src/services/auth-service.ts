7568670195e2aaa57bdfeeba7eeea4519473dec0
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-31 21:52:36 +0330

    Subject: Debug, Description: Edit for data intry types and errors, Date: 12/31/2023

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,19 +1,36 @@
 import http from "./http-service";
 
-export function getOtp(data:any) {
-  return http.post("/user/get-otp", data).then(({ data }) => data.data).catch((err)=>{
-    console.log(err);
-    
-    return err
-  });
+interface getOtpDataType {
+  phoneNumber: string;
 }
 
-export function checkOtp(data:any) {
-  return http.post("/user/check-otp", data).then(({ data }) => data.data);
+export function getOtp(data: getOtpDataType): Promise<any> {
+  return http
+    .post("/user/get-otp", data)
+    .then(({ data }) => data.data)
+    .catch((error) => {
+      return Promise.reject(error);
+    });
 }
 
-export function completeProfile(data:any) {
-  return http.post("/user/complete-profile", data).then(({ data }) => data.data);
+interface checkOtpDataType {
+  phoneNumber: string;
+  otp: string;
+}
+
+export function checkOtp(data: checkOtpDataType): Promise<any> {
+  return http
+    .post("/user/check-otp", data)
+    .then(({ data }) => data.data)
+    .catch((error) => {
+      return Promise.reject(error);
+    });
+}
+
+export function completeProfile(data: any) {
+  return http
+    .post("/user/complete-profile", data)
+    .then(({ data }) => data.data);
 }
 
 export function getUser() {
