5b8eb297f9abe94f49deff02d2c49de836f993c7
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-01 21:15:58 +0330

    Subject: Freelancer app, Description: Edit table row for showing modal, Date: 1/1/2023

--- src/features/projects/table/components/project-row.tsx
+++ src/features/projects/table/components/project-row.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { HiOutlineTrash } from "react-icons/hi";
 import { TbPencilMinus } from "react-icons/tb";
+import Modal from "../../../../common/modal";
 import Table from "../../../../common/table";
 import toLocalDateShort from "../../../../utils/to-local-date-short";
 import { toPersianNumbersWithComma } from "../../../../utils/to-persian-numbers";
@@ -12,6 +14,7 @@ export default function ProjectRow({
   project: any;
   index: number;
 }) {
+  const [isEditOpen, setIsEditOpen] = useState(false);
   return (
     <>
       <Table.Row key={project.id}>
@@ -31,19 +34,28 @@ export default function ProjectRow({
         </td>
         <td>{project.freelancer?.name || "-"}</td>
         <td>
-          {project.staatus == "OPEN" ? (
+          {project.status == "OPEN" ? (
             <span className="badge badge--success">باز</span>
           ) : (
             <span className="badge badge--danger">بسته</span>
           )}
         </td>
         <td>
-          <button>
-            <TbPencilMinus className="w-5 h-5 text-primary-" />
-          </button>
-          <button>
-            <HiOutlineTrash />
-          </button>
+          <div className="flex items-center gap-x-4">
+            <button onClick={() => setIsEditOpen(true)}>
+              <TbPencilMinus className="w-5 h-5 text-primary-900" />
+            </button>
+            <Modal
+              open={isEditOpen}
+              title="modal title"
+              onClose={() => setIsEditOpen(false)}
+            >
+              This is modal
+            </Modal>
+            <button>
+              <HiOutlineTrash className="w-5 h-5 text-error" />
+            </button>
+          </div>
         </td>
       </Table.Row>
     </>
