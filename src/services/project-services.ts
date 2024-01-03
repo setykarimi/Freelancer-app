3dee1da93ed8d2e6c4836af71fe5439d5ca02997
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-03 19:14:15 +0330

    Subject: Projects, Description: Edit content, Date: 1/3/2023

--- src/services/project-services.ts
+++ src/services/project-services.ts
@@ -4,6 +4,14 @@ export function getOwnerProjectsApi() {
   return http.get("/project/owner-projects").then(({ data }) => data.data);
 }
 
-export function removeProjectAPi(id: string){
-  return http.get(`/project/${id}`).then(({ data }) => data.data);
+export function removeProjectAPi(id: string): Promise<any>{
+  return http.get(`/project/${id}`).then(({ data }) => {
+    console.log("data", data);
+    return data.data
+    
+  }).catch((error)=> {
+    console.log("error", error);
+    
+    return Promise.reject(error);
+  })
 }
