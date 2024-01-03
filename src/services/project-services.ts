acf0b8fd6f2998e13f5185452ff6e035c014d1ad
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-03 19:05:32 +0330

    Subject: Projects, Description: Add remove api, Date: 1/3/2023

--- src/services/project-services.ts
+++ src/services/project-services.ts
@@ -3,3 +3,7 @@ import http from "./http-service";
 export function getOwnerProjectsApi() {
   return http.get("/project/owner-projects").then(({ data }) => data.data);
 }
+
+export function removeProjectAPi(id: string){
+  return http.get(`/project/${id}`).then(({ data }) => data.data);
+}
