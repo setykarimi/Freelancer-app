d20f781a9b43340164535bfeeddb53143cf3a82a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-07 21:10:52 +0330

    Subject: Config, Description: Edit for importing component, Date: 1/7/2023

--- src/common/layout/components/header.tsx
+++ src/common/layout/components/header.tsx
@@ -1,6 +1,4 @@
-import useUser from "../../../hook/use-user";
 
 export default function Navbar() {
-  const { data } = useUser();
   return <div className="bg-secondary-0 py-4 px-8 border-b border-b-secondary-100">App header</div>;
 }
