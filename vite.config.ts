9c15cc0f8f1f6422dd79a860c59276e7336657f8
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-07 21:10:20 +0330

    Subject: Config, Description: Add alias, Date: 1/7/2023

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port: 3000
-  }
-})
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@common": "/src/common",
+      "@features": "/src/features",
+      "@hook": "/src/hook",
+      "@pages": "/src/pages",
+      "@services": "/src/services",
+      "@utils": "/src/utils",
+    },
+  },
+});
