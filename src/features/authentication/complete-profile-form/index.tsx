4872db14f2f9f05ee0450fb1037c79cfba06773d
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-08 21:51:52 +0330

    Subject: Projects, Description: Convert form to react hook form, Date: 1/8/2023

--- src/features/authentication/complete-profile-form/index.tsx
+++ src/features/authentication/complete-profile-form/index.tsx
@@ -1,72 +1,79 @@
 import Loading from "@common/loading";
-import RadioInput from "@common/radio-input";
 import TextField from "@common/text-field";
 import { completeProfile } from "@services/auth-service";
 import { useMutation } from "@tanstack/react-query";
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
+import RadioInputGroup from "./components/radio-input-group";
 
 export default function CompleteProfileForm() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    watch,
+  } = useForm();
   const { mutateAsync, isPending } = useMutation({
     mutationFn: completeProfile,
   });
 
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    role: "",
-  });
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value }: { name: string; value: string } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async () => {
+  const onSubmit = async (values: any) => {
     try {
-      const { message } = await mutateAsync(formData);
+      const { message } = await mutateAsync(values);
       toast.success(message);
     } catch (error: any) {
       toast.error(error?.response?.data?.message);
     }
   };
 
+  console.log(errors["role"]?.message);
+
   return (
     <div className="w-full sm:max-w-sm mx-auto pt-10">
-      <form className="space-y-8" onSubmit={handleSubmit}>
+      <form className="space-y-8" onSubmit={handleSubmit(onSubmit)}>
         <TextField
           label="نام و نام خانوادگی"
           name="name"
-          value={formData.name}
-          onChange={onChange}
+          errors={errors}
+          register={register}
+          validationSchema={{
+            required: "نام و نام خانوادگی ضروری است.",
+          }}
+          required
         />
         <TextField
           label="ایمیل"
           name="email"
-          value={formData.email}
-          onChange={onChange}
+          errors={errors}
+          register={register}
+          validationSchema={{
+            required: "ایمیل ضروری است.",
+            pattern: {
+              value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+              message: "ایمیل نامعتبر است.",
+            },
+          }}
+          required
         />
-        <div className="flex items-center justify-center gap-x-8">
-          <RadioInput
-            label="کارفرما"
-            name="role"
-            id="OWNER"
-            value="OWNER"
-            onChange={onChange}
-            checked={formData.role == "OWNER"}
-          />
-          <RadioInput
-            label="فریلنسر"
-            name="role"
-            id="FREELANCER"
-            value="FREELANCER"
-            onChange={onChange}
-            checked={formData.role == "FREELANCER"}
-          />
-        </div>
+        <RadioInputGroup
+          register={register}
+          watch={watch}
+          errors={errors}
+          config={{
+            name: "role",
+            validationSchema: { required: "انتخاب نقش ضروری است" },
+            option: [
+              {
+                value: "OWNER",
+                label: "کارفرما",
+              },
+              {
+                value: "FREELANCE",
+                label: "فریلنسر",
+              },
+            ],
+          }}
+        />
         {isPending ? (
           <Loading />
         ) : (
