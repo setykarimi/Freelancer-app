a2725e685fa9eb99890fe4579c0e38d1b2c7c3b1
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-08 21:50:33 +0330

    Subject: Projects, Description: Add form modal, Date: 1/8/2023

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,5 +1,31 @@
+import Modal from "@common/modal";
+import CreateProjectForm from "@features/projects/form/create-project";
 import PorjectTable from "@features/projects/table";
+import { useState } from "react";
+import { HiPlus } from "react-icons/hi";
 
 export default function Projects() {
-  return <PorjectTable />;
+  const [openModal, setOpenModal] = useState(false);
+  return (
+    <div>
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-secondary-700 font-bold text-xl">پروژه‌های شما</h1>
+        <button
+          className="btn btn--primary flex items-center gap-2"
+          onClick={() => setOpenModal(true)}
+        >
+          <HiPlus />
+          افزودن پروژه جدید
+        </button>
+        <Modal
+          open={openModal}
+          title="اضافه کردن پروژه جدید"
+          onClose={() => setOpenModal(false)}
+        >
+          <CreateProjectForm />
+        </Modal>
+      </div>
+      <PorjectTable />
+    </div>
+  );
 }
