4021fb56dcab1b78cfc9d2b0b7492c5b8bda0ca4
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-08 21:50:48 +0330

    Subject: Projects, Description: Add Title, Date: 1/8/2023

--- src/pages/complete-profile/index.tsx
+++ src/pages/complete-profile/index.tsx
@@ -3,6 +3,7 @@ import CompleteProfileForm from "@features/authentication/complete-profile-form"
 export default function CompleteProfile() {
   return (
     <div className="container xl:max-w-screen-xl">
+      <h1 className="pt-16 text-center font-bold text-secondary-700 text-3xl">تکمیل اطلاعات</h1>
       <CompleteProfileForm />
     </div>
   );
