6cc5d990f0261ac121082b0520dcca101f300f08
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-09 22:07:29 +0330

    Subject: Projects, Description: Add createProjectApi, Date: 1/9/2023

--- src/services/project-services.ts
+++ src/services/project-services.ts
@@ -4,10 +4,16 @@ export function getOwnerProjectsApi() {
   return http.get("/project/owner-projects").then(({ data }) => data.data);
 }
 
-export function removeProjectApi(id: string): Promise<any>{
-  return http.delete(`/project/${id}`).then(({ data }) => {
-    return data.data
-  }).catch((error)=> {    
-    return Promise.reject(error);
-  })
+export function removeProjectApi(id: string): Promise<any> {
+  return http
+    .delete(`/project/${id}`)
+    .then(({ data }) => data.data)
+    .catch((error) => Promise.reject(error));
+}
+
+export function createProjectApi(postData: any): Promise<any> {
+  return http
+    .post(`/project/add`, postData)
+    .then(({ data }) => data.data)
+    .catch((error) => Promise.reject(error));
 }
