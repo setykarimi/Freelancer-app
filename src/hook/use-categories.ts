5b23923fea05b6607154028bcb8f59f8161287f9
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-09 21:50:25 +0330

    Subject: Projects, Description: Create new hook for categorie, Date: 1/9/2023

---
+++ src/hook/use-categories.ts
@@ -0,0 +1,21 @@
+import { getCategoryApi } from "@services/category-service";
+import { useQuery } from "@tanstack/react-query";
+
+export default function useCategories() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["categories"],
+    queryFn: getCategoryApi,
+  });
+
+  const { categories: rawCategories = [] } = data || {};
+  const categories = rawCategories.map((item: any) => ({
+    label: item.title,
+    value: item._id,
+  }));
+  const newCategories = rawCategories.map((item: any) => ({
+    label: item.title,
+    value: item.englishTitle,
+  }));
+
+  return { categories, newCategories, isLoading };
+}
