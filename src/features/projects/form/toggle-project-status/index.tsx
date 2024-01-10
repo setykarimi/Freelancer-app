de6451ec87f345492d38553189eb3a70fa283b83
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-10 22:06:50 +0330

    Subject: Projects, Description: Create new component for toggle project status, Date: 1/10/2023

---
+++ src/features/projects/form/toggle-project-status/index.tsx
@@ -0,0 +1,30 @@
+import Toggle from "@common/form/toggle";
+import Loading from "@common/loading";
+import useToggleProjectStatus from "@hook/use-toggle-project-status";
+
+export default function ToggleProjectStatus({ project }: { project: any }) {
+  const { isUpdating, toggleProject } = useToggleProjectStatus();
+  const enabled = project.status === "OPEN" ? true : false;
+  const toggleHandler = () => {
+    const status = project.status === "OPEN" ? "CLOSED" : "OPEN";
+
+    toggleProject({
+      id: project._id,
+      postData: { status },
+    });
+  };
+
+  return (
+    <div className="w-5rem">
+      {isUpdating ? (
+        <Loading height={20} width={50} />
+      ) : (
+        <Toggle
+          label={project.status === "OPEN" ? "باز" : "بسته"}
+          onChange={toggleHandler}
+          enabled={enabled}
+        />
+      )}
+    </div>
+  );
+}
