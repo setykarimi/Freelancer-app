08f4e366324a807c15b0576aac8e2f6d3d76959e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-11 11:19:43 +0330

    Subject: Projects, Description: Add real data in project page, Date: 1/11/2023

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,3 +1,18 @@
+import Loading from "@common/loading";
+import ProjectHeader from "@features/project/proposal/header";
+import ProjectProposal from "@features/project/proposal/table";
+import useProject from "@hook/use-project";
+
 export default function Project() {
-  return <div>Project</div>;
+  const { isLoading, project } = useProject();
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <ProjectHeader project={project} />
+      <ProjectProposal proposal={project?.proposals} />
+    </div>
+  );
 }
