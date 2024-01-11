cbdcca0d6967a4bd6bc4363185d5721158d44347
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-11 17:54:33 +0330

    Subject: header, Description: Edit hook, Date: 1/11/2023

--- src/hook/use-user.ts
+++ src/hook/use-user.ts
@@ -1,10 +1,17 @@
 import { getUser } from "@services/auth-service";
 import { useQuery } from "@tanstack/react-query";
 
-export default function useUser(){
-    return useQuery({
-        queryKey:["get-user"],
-        queryFn: getUser,
-        retry: false,
-    })
+export default function useUser() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["user"],
+    queryFn: getUser,
+    retry: false,
+  });
+
+  const { user } = data || {};
+
+  return {
+    isLoading,
+    user,
+  };
 }
