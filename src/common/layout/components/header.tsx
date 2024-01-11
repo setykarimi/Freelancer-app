6355b9245080624f30ea294c2432e408ca687326
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-11 17:55:30 +0330

    Subject: header, Description: Add Avatar component, Date: 1/11/2023

--- src/common/layout/components/header.tsx
+++ src/common/layout/components/header.tsx
@@ -1,4 +1,17 @@
+import useUser from "@hook/use-user";
+import Avatar from "./header/avatar";
 
 export default function Navbar() {
-  return <div className="bg-secondary-0 py-4 px-8 border-b border-b-secondary-100">App header</div>;
+  const { isLoading, user } = useUser();
+  return (
+    <div className="bg-secondary-0 py-4 px-8 border-b border-b-secondary-200">
+      <div
+        className={`container xl:max-w-screen-lg flex items-center justify-end ${
+          isLoading ? "blur-sm opacity-50" : ""
+        }`}
+      >
+        <Avatar />
+      </div>
+    </div>
+  );
 }
