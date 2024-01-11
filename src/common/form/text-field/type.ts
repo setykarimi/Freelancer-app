a0c467b7783f9f59a968ee339ec3888448999c8f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-11 16:40:33 +0330

    Subject: Type, Description: Edit for types, Date: 1/11/2023

--- src/common/form/text-field/type.ts
+++ src/common/form/text-field/type.ts
@@ -1,7 +1,9 @@
+import { FieldValues, UseFormRegister } from "react-hook-form";
+
 export interface TextFieldPropsType {
   label: string;
   name: string;
-  register: any;
+  register: UseFormRegister<FieldValues>;
   type?: string;
   required?: boolean;
   validationSchema: any;
