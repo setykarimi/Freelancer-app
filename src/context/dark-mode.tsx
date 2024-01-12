18af78fc5255b17d2854773677bc9db279beae04
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-12 13:11:30 +0330

    Subject: header, Description: Create new folder for context, Date: 1/12/2023

---
+++ src/context/dark-mode.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const DarkModeContext = createContext();
+
+export function DarkModeProvider({ children }: { children: React.ReactNode }) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => setIsDarkMode((prv) => !prv);
+
+  useEffect(()=> {
+    if(isDarkMode){
+      document.documentElement.classList.add('dark-mode')
+      document.documentElement.classList.remove('light-mode')
+    } else {
+      document.documentElement.classList.remove('dark-mode')
+      document.documentElement.classList.add('light-mode')
+    }
+  },[isDarkMode])
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+export function useDarkMode() {
+  const context = useContext(DarkModeContext);
+
+  if (context === undefined)
+    throw new Error("DarkModeContext was used outside of DarkModeProvider");
+
+  return context;
+}
