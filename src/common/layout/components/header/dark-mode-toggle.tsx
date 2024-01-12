e06b7e1cbda5c15083762f77746bae0f9537c6d7
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-12 12:39:28 +0330

    Subject: header, Description: Create new component for dark mode toggle, Date: 1/12/2023

---
+++ src/common/layout/components/header/dark-mode-toggle.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HiOutlineSun } from 'react-icons/hi'
+
+export default function DarkModeToggle() {
+  return (
+    <button>
+        <HiOutlineSun className="w-5 h-5 text-secondary-500"/>
+    </button>
+  )
+}
