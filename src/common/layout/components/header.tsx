d5d2a69538385a799ca1217541674f606bf9acff
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-12 12:40:12 +0330

    Subject: header, Description: Edit style, Date: 1/12/2023

--- src/common/layout/components/header.tsx
+++ src/common/layout/components/header.tsx
@@ -1,16 +1,18 @@
 import useUser from "@hook/use-user";
 import Avatar from "./header/avatar";
+import HeaderMenu from "./header/menu";
 
 export default function Navbar() {
   const { isLoading, user } = useUser();
   return (
     <div className="bg-secondary-0 py-4 px-8 border-b border-b-secondary-200">
       <div
-        className={`container xl:max-w-screen-lg flex items-center justify-end ${
+        className={`container xl:max-w-screen-lg flex items-center justify-end gap-x-4 ${
           isLoading ? "blur-sm opacity-50" : ""
         }`}
       >
         <Avatar />
+        <HeaderMenu />
       </div>
     </div>
   );
