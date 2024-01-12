d3537a47ef0bc53c33c4446a0d7c8a92812c3ecd
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-12 12:39:44 +0330

    Subject: header, Description: Add logout api calling, Date: 1/12/2023

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -36,3 +36,7 @@ export function completeProfile(data: any) {
 export function getUser() {
   return http.get("/user/profile").then(({ data }) => data.data);
 }
+
+export function logoutApi() {
+  return http.post("/user/logout").then(({ data }) => data.data);
+}
