ea264bd90f7d7c1d8f1f14d7df89d588bffc156f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-12 13:12:07 +0330

    Subject: header, Description: change mode handler, Date: 1/12/2023

--- src/common/layout/components/header/dark-mode-toggle.tsx
+++ src/common/layout/components/header/dark-mode-toggle.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import { HiOutlineSun } from 'react-icons/hi'
+import { useDarkMode } from "@context/dark-mode";
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi";
 
 export default function DarkModeToggle() {
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
   return (
-    <button>
-        <HiOutlineSun className="w-5 h-5 text-secondary-500"/>
+    <button onClick={toggleDarkMode}>
+      {isDarkMode ? (
+        <HiOutlineSun className="w-5 h-5 text-secondary-500" />
+      ) : (
+        <HiOutlineMoon className="w-5 h-5 text-secondary-500" />
+      )}
     </button>
-  )
+  );
 }
