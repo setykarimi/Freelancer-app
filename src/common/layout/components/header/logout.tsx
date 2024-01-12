51eacc37f451960c1f38bcfc722ca62a86dc30a0
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-12 12:39:14 +0330

    Subject: header, Description: Create new component for logout, Date: 1/12/2023

---
+++ src/common/layout/components/header/logout.tsx
@@ -0,0 +1,19 @@
+import Loading from "@common/loading";
+import useLogout from "@hook/use-logout";
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+
+export default function Logout() {
+  const { isPending, logout } = useLogout();
+
+  const logoutHandler = () => {
+    logout();
+  };
+
+  return isPending ? (
+    <Loading />
+  ) : (
+    <button onClick={logoutHandler}>
+      <HiArrowRightOnRectangle className="w-5 h-5 text-secondary-500 hover:text-error" />
+    </button>
+  );
+}
