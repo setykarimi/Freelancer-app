fc21620b38afa6748b08a081fa161d4ff6690d17
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-12 12:39:59 +0330

    Subject: header, Description: Add new component, Date: 1/12/2023

--- src/common/layout/components/header/menu.tsx
+++ src/common/layout/components/header/menu.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import DarkModeToggle from "@common/layout/components/header/dark-mode-toggle";
+import { HiOutlineUser } from "react-icons/hi";
+import { Link } from "react-router-dom";
+import Logout from "./logout";
 
 export default function HeaderMenu() {
   return (
-    <div>HeaderMenu</div>
-  )
+    <ul className="flex gap-x-4 items-center">
+      <li className="flex items-center justify-center">
+        <Link to="dashboard">
+          <HiOutlineUser classsName="w-5 h-5 text-primary-900" />
+        </Link>
+      </li>
+      <li className="flex items-center justify-center">
+        <DarkModeToggle />
+      </li>
+      <li className="flex items-center justify-center"><Logout /></li>
+    </ul>
+  );
 }
