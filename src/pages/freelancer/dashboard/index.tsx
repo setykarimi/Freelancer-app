386b9b577c114f382e2cf7638d125e828dfacff8
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-14 22:05:37 +0330

    Subject: freelancer, Description: Create new folder for freelancer routes, Date: 1/14/2023

---
+++ src/pages/freelancer/dashboard/index.tsx
@@ -0,0 +1,6 @@
+
+export default function FreelancerDashboard() {
+  return (
+    <div>FreelancerDashboard</div>
+  )
+}
---
+++ src/pages/freelancer/proposals/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FreelancerProposals() {
+  return (
+    <div>Proposals</div>
+  )
+}
