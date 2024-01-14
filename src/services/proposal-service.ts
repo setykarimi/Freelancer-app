716f5cb6aceaaaa0bb5981fdd85d62f589ce0b95
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-14 22:29:32 +0330

    Subject: freelancer, Description: Add new api calling, Date: 1/14/2023

--- src/services/proposal-service.ts
+++ src/services/proposal-service.ts
@@ -1,10 +1,15 @@
 import http from "./http-service";
 
-export function changeProposalStatusApi({id,postData}: any): Promise<any> {
-    console.log(postData);
-    
-    return http
-      .patch(`/proposal/${id}`, postData)
-      .then(({ data }) => data.data)
-      .catch((error) => Promise.reject(error));
-  }
+export function changeProposalStatusApi({ id, postData }: any): Promise<any> {
+  return http
+    .patch(`/proposal/${id}`, postData)
+    .then(({ data }) => data.data)
+    .catch((error) => Promise.reject(error));
+}
+
+export function getProposalsApi(): Promise<any> {
+  return http
+    .get(`/proposal/list`)
+    .then(({ data }) => data.data)
+    .catch((error) => Promise.reject(error));
+}
