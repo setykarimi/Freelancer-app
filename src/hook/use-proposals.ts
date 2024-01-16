9bc33da8a521218809a1ea45d0a00c542fdc348b
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-16 22:36:22 +0330

    Subject: freelancer, Description: Edit data, Date: 1/16/2023

--- src/hook/use-proposals.ts
+++ src/hook/use-proposals.ts
@@ -1,13 +1,13 @@
 import { getProposalsApi } from "@services/proposal-service";
 import { useQuery } from "@tanstack/react-query";
 
-export default function useProposals(){
-    const {data, isLoading} = useQuery({
-        queryKey: ["proposals"],
-        queryFn: getProposalsApi
-    })
+export default function useProposals() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["proposals"],
+    queryFn: getProposalsApi,
+  });
 
-    const {proposals} = data || {}
+  const { proposals } = data || {};
 
-    return {proposals, isLoading}
+  return { proposals, isLoading };
 }
