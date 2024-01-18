97ec4748ce47cdd877bcd7aa8393a3d1b8541921
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-18 10:00:16 +0330

    Subject: filter, Description: Edit filter drop down for searchParams, Date: 1/18/2023

--- src/common/form/filter/drop-down.tsx
+++ src/common/form/filter/drop-down.tsx
@@ -12,18 +12,22 @@ export default function FilterDropDown({
   const filterValue = searchParams.get(filterField) || "";
 
   function handleChange(e: any) {
+    console.log(e.target.value);
+    
     searchParams.set(filterField, e.target.value);
     setSearchParams(searchParams);
   }
 
+  
+
   return (
     <select
-      value={filterField}
+      value={filterValue}
       onChange={handleChange}
       className="textField__input py-2 text-xs bg-secondary-0"
     >
       {options.map((item) => (
-        <option key={item.value} value={filterValue}>
+        <option key={item.value} value={item.value}>
           {item.label}
         </option>
       ))}
