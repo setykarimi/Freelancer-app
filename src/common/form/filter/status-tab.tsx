174b4147ffeeb5a046051780f182d71c6b4037c3
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-18 09:58:28 +0330

    Subject: filter, Description: Create new filter for status, Date: 1/18/2023

---
+++ src/common/form/filter/status-tab.tsx
@@ -0,0 +1,41 @@
+import { useSearchParams } from "react-router-dom";
+
+export default function StatusFilter({
+  filterField,
+  options,
+}: {
+  filterField: string;
+  options: any;
+}) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
+
+  function handleChange(value: string) {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams);
+  }
+  return (
+    <div className="flex items-center gap-x-2 text=xs">
+      <span>وضعیت</span>
+      <div className="flex items-center gap-x-2 border border-secondary-100 bg-secondary-0 rounded-lg">
+        {options.map(({ value, label }: { value: string; label: string }) => {
+          const isActive = value === currentFilter;
+          return (
+            <button
+              key={value}
+              disabled={isActive}
+              onClick={() => handleChange(value)}
+              className={`whitespace-nowrap rounded-md px-4 py-2 font-bold transition-all duration-300 text-xs ${
+                isActive
+                  ? "!bg-primary-900 text-white"
+                  : "bg-secondary-0 text-secondary-800"
+              }`}
+            >
+              {label}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
