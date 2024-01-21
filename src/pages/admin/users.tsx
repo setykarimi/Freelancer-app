77d8c8d1c631220c1830b395f0c84671bf958096
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-21 22:48:26 +0330

    Subject: Admin, Description: Create new page for users, Date: 1/21/2023

---
+++ src/pages/admin/users.tsx
@@ -0,0 +1,10 @@
+import UsersTable from "@features/admin/users/table";
+
+export default function AdminUsers() {
+  return (
+    <div>
+      <h1 className="font-black text-secondary-700 text-xl mb-8">کاربران</h1>
+      <UsersTable />
+    </div>
+  );
+}
