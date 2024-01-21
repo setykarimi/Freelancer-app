93b65ce1119576f69583fbed86a3f4488309c7bb
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-21 22:50:10 +0330

    Subject: Admin, Description: Edit file, Date: 1/21/2023

--- src/hook/use-change-proposal-status.ts
+++ src/hook/use-change-proposal-status.ts
@@ -1,15 +1,15 @@
-import { changeProposalStatusApi } from "@services/proposal-service";
+import { changeUserStatusApi } from "@services/auth-service";
 import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 export default function useChangeProposalStatus() {
-  const { isPending: isEditing, mutate: changeProposalStatus } = useMutation({
-    mutationFn: changeProposalStatusApi,
+  const { isPending: isEditing, mutate: changeUserStatus } = useMutation({
+    mutationFn: changeUserStatusApi,
     onSuccess: (data) => {
       toast.success(data.message);
     },
     onError: (err: any) => toast.error(err?.response?.data?.message),
   });
 
-  return { isEditing, changeProposalStatus };
+  return { isEditing, changeUserStatus };
 }
