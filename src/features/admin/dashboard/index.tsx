11850e91fca8addb439c1f9f3a1a0d487c970945
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-21 22:50:17 +0330

    Subject: Admin, Description: Edit file, Date: 1/21/2023

--- src/features/admin/dashboard/index.tsx
+++ src/features/admin/dashboard/index.tsx
@@ -1,5 +1,19 @@
+import Loading from "@common/loading";
+import useProjects from "@hook/use-projects";
+import useProposals from "@hook/use-proposals";
+import useUsers from "@hook/use-users";
 import DashboardHeader from "./components/header";
+import Stats from "../stats";
 
 export default function AdminDashboard() {
-  return <DashboardHeader />;
+  const { isLoading: isLoading1, proposals } = useProposals();
+  const { isLoading: isLoading2, projects } = useProjects();
+  const { isLoading: isLoading3, users } = useUsers();
+
+  if (isLoading1 || isLoading2 || isLoading3) return <Loading />;
+
+  return <>
+  <DashboardHeader />
+  <Stats proposals={proposals.length} projects={projects.length} users={users.length}/>
+  </>;
 }
