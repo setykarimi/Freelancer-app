9fa33662f86c9fe767cc71c22c5e9b8990610bcd
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-22 22:22:55 +0330

    Subject: Admin, Description: Edit for authorization, Date: 1/22/2023

--- src/common/protected-route/index.tsx
+++ src/common/protected-route/index.tsx
@@ -1,16 +1,21 @@
 import Loading from "@common/loading";
 import useAuthorize from "@hook/use-authorize";
 import { useEffect } from "react";
+import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
   const navigate = useNavigate();
-  const { isAuthorized, isLoading, isAuthenticated } = useAuthorize();
+  const { isAuthorized, isLoading, isAuthenticated, isVerfied } = useAuthorize();
 
   useEffect(() => {
     if (!isAuthenticated && !isLoading) navigate("/auth");
+    if (!isVerfied &&  !isLoading) {
+      toast.error("پروفایل شما هنوز تایید نشده است.")
+      navigate("/not-access")
+    };
     if (!isAuthorized && !isLoading) navigate("/not-access");
-  }, [isAuthenticated, isAuthorized, isLoading]);
+  }, [isAuthenticated, isAuthorized, isLoading, isVerfied]);
 
   if (isLoading) {
     return (
