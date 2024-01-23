b6b20617115876327ad703f07ecd96a9cad04da6
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-23 22:18:30 +0330

    Subject: Authentication, Description: Edit route, Date: 1/22/2023

--- src/App.tsx
+++ src/App.tsx
@@ -64,8 +64,7 @@ function App() {
             <Route path="dashboard" element={<AdminDashboardPage />} />
             <Route path="users" element={<AdminUsers />} />
             <Route path="projects" element={<SubmittedProjects />} />
-            <Route path="proposals
-            " element={<FreelancerProposals />} />
+            <Route path="proposals" element={<FreelancerProposals />} />
           </Route>
           <Route path="*" element={<h1>صفحه‌ای که دنبالش هستید یافت نشد</h1>} />
         </Routes>
