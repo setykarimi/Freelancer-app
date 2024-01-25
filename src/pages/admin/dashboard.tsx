44b4093ac1349c244e614ded1094fd60837cad57
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-01-25 09:52:26 +0330

    Subject: Dashboard, Description: Edit folder structure for admin dashboard, Date: 1/25/2023

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,5 +1,25 @@
-import AdminDashboard from "@features/admin/dashboard";
+import Loading from "@common/loading";
+import DashboardHeader from "@features/dashboard/header";
+import Stats from "@features/dashboard/stats";
+import useProjects from "@hook/use-projects";
+import useProposals from "@hook/use-proposals";
+import useUsers from "@hook/use-users";
 
 export default function AdminDashboardPage() {
-  return <AdminDashboard />;
+  const { isLoading: isLoading1, proposals } = useProposals();
+  const { isLoading: isLoading2, projects } = useProjects();
+  const { isLoading: isLoading3, users } = useUsers();
+
+  if (isLoading1 || isLoading2 || isLoading3) return <Loading />;
+
+  return (
+    <>
+      <DashboardHeader />
+      <Stats
+        proposals={proposals.length}
+        projects={projects.length}
+        users={users.length}
+      />
+    </>
+  );
 }
