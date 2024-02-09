1a0628b2416daecfb793255d6e34c4128a623347
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-09 20:01:50 +0330

    Subject: Debug, Description: Edit for typescript errors, Date: 2/9/2024

--- src/features/owner/stat.tsx
+++ src/features/owner/stat.tsx
@@ -1,12 +1,24 @@
+import React from "react";
+
 const colors = {
   primary: "bg-primary-100 text-primary-700",
   green: "bg-green-100 text-green-700",
   yellow: "bg-yellow-100 text-yellow-700",
 };
-export default function Stat({ icon, value, title, color }) {
+
+type StateType = {
+  icon: React.ReactNode;
+  value: number | string;
+  title: string;
+  color: "primary" | "green" | "yellow";
+};
+
+export default function Stat({ icon, value, title, color }: StateType) {
   return (
     <div className="grid grid-rows-2 grid-cols-[6.4rem_1fr] bg-secondary-0 p-4 rounded-lg gap-x-4 ">
-      <div className={`row-span-2 flex items-center justify-center p-2 aspect-square rounded-full ${colors[color]}`}>
+      <div
+        className={`row-span-2 flex items-center justify-center p-2 aspect-square rounded-full ${colors[color]}`}
+      >
         {icon}
       </div>
       <h5 className="font-bold text-lg text-secondary-500 self-center">
