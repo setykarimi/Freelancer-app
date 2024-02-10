19305bc7416b8f3e62a7e700c2f7fb5961b54757
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-10 21:35:26 +0330

    Subject: Freelancer app, Description: Add next version for freelancer app, Date: 2/10/2024

--- next/src/services/auth-service.ts
+++
@@ -1,5 +0,0 @@
-import http from "./http-service";
-
-export function getUser() {
-    return http.get("/user/profile").then(({ data }) => data.data);
-  }
--- next/src/utils/provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
-
-export default function Provider({ children }: React.PropsWithChildren) {
-  const [client] = React.useState(new QueryClient());
-
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
-}
