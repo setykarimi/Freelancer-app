ad1338d58b92ff9793c6a2f89d6224329ff48819
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-11 12:08:01 +0330

    Subject: Freelancer app, Description: Add Toaaster, Date: 2/11/2024

--- next-folder/src/utils/provider.tsx
+++ next-folder/src/utils/provider.tsx
@@ -1,9 +1,15 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
+import { Toaster } from "react-hot-toast";
 
 export default function Provider({ children }: React.PropsWithChildren) {
   const [client] = React.useState(new QueryClient());
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      <Toaster />
+      {children}
+    </QueryClientProvider>
+  );
 }
