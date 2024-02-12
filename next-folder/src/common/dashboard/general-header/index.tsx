943400daf151efa9bae226db8609224444d31ff5
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-12 22:05:46 +0330

    Subject: Dashboard header, Description: Create new component for dashboard header, Date: 2/12/2024

---
+++ next-folder/src/common/dashboard/general-header/index.tsx
@@ -0,0 +1,11 @@
+export default function GeneralHeader() {
+    return (
+      <div className="mb-8">
+        <h1 className="font-bold text-2xl text-secondary-900 mb-4">آمار کلی</h1>
+        <p className="text-secondary-600">
+          در یک نما خلاصه‌ای از آمار خود را ببینید
+        </p>
+      </div>
+    );
+  }
+  
