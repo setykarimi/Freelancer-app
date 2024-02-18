6d95657e1bbcef87fa8342818d55c848156964c7
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-18 21:36:42 +0330

    Subject: Dashboard, Description: Create new component for permissions, Date: 2/18/2024

---
+++ next-folder/src/utils/permissions.tsx
@@ -0,0 +1,57 @@
+import { HiCollection, HiDocumentText, HiHome, HiUser } from "react-icons/hi";
+
+const permissions = {
+  ADMIN: [
+    {
+      name: "داشبورد",
+      to: "/dashboard/admin",
+      icon: <HiHome size={20} />,
+    },
+    {
+      name: "کاربران",
+      to: "users",
+      icon: <HiUser size={20} />,
+    },
+    {
+      name: "پروژه‌ها",
+      to: "projects",
+      icon: <HiCollection size={20} />,
+    },
+    {
+      name: "درخواست‌ها",
+      to: "proposals",
+      icon: <HiCollection size={20} />,
+    },
+  ],
+  FREELANCER: [
+    {
+      name: "داشبورد",
+      to: "dashboard",
+      icon: <HiHome size={20} />,
+    },
+    {
+      name: "پروژه‌ها",
+      to: "projects",
+      icon: <HiCollection size={20} />,
+    },
+    {
+      name: "درخواست‌ها",
+      to: "proposals",
+      icon: <HiCollection size={20} />,
+    },
+  ],
+  OWNER: [
+    {
+      name: "خانه",
+      to: "/owner/dashboard",
+      icon: <HiHome size={20} />,
+    },
+    {
+      name: "پروژه‌ها",
+      to: "/owner/projects",
+      icon: <HiDocumentText size={20} />,
+    },
+  ],
+};
+
+export default permissions;
