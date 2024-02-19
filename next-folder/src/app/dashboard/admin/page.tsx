64fedf433f7bc7db4d1388e1924e8d5fc15f7787
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-19 23:24:56 +0330

    Subject: Dashboard, Description: Add use proposal query, Date: 2/19/2024

--- next-folder/src/app/dashboard/admin/page.tsx
+++ next-folder/src/app/dashboard/admin/page.tsx
@@ -1,8 +1,15 @@
+"use client"
 import GeneralHeader from '@/common/dashboard/general-header'
-import React from 'react'
+import useProposals from '@/hooks/proposals/use-proposals';
 
 export default function AdminMainPage() {
+  const { isLoading: isLoading1, proposals } = useProposals();
+  // const { isLoading: isLoading2, projects } = useProjects();
+  // const { isLoading: isLoading3, users } = useUsers();
+
   return (
-    <GeneralHeader />
-  )
+    <>
+      <GeneralHeader />
+    </>
+  );
 }
