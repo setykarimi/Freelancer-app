6f5f71d531c3c83aca442e2b1d8c10fd21eabb99
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-19 23:23:47 +0330

    Subject: Dashboard, Description: Create new service for proposal services, Date: 2/19/2024

---
+++ next-folder/src/services/proposal-service.ts
@@ -0,0 +1,22 @@
+import http from "./http-service";
+
+export function changeProposalStatusApi({ id, postData }: any): Promise<any> {
+  return http
+    .patch(`/proposal/${id}`, postData)
+    .then(({ data }) => data.data)
+    .catch((error) => Promise.reject(error));
+}
+
+export function getProposalsApi(): Promise<any> {
+  return http
+    .get(`/proposal/list`)
+    .then(({ data }) => data.data)
+    .catch((error) => Promise.reject(error));
+}
+
+export function createProposalApi(postData: any): Promise<any> {
+  return http
+    .post(`/proposal/add`, postData)
+    .then(({ data }) => data.data)
+    .catch((error) => Promise.reject(error));
+}
