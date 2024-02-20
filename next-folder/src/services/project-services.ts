1847fb810c8afc58351d9b9b0f62a2603522bd54
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-20 21:53:11 +0330

    Subject: Dashboard, Description: Create projects services, Date: 2/20/2024

---
+++ next-folder/src/services/project-services.ts
@@ -0,0 +1,46 @@
+import http from "./http-service";
+
+export function getOwnerProjectsApi() {
+  return http.get("/project/owner-projects").then(({ data }) => data.data);
+}
+
+export function removeProjectApi(id: string): Promise<any> {
+  return http
+    .delete(`/project/${id}`)
+    .then(({ data }) => data.data)
+    .catch((error) => Promise.reject(error));
+}
+
+export function createProjectApi(postData: any): Promise<any> {
+  return http
+    .post(`/project/add`, postData)
+    .then(({ data }) => data.data)
+    .catch((error) => Promise.reject(error));
+}
+
+export function editProjectApi({ id, postData }: any): Promise<any> {
+  return http
+    .patch(`/project/update/${id}`, postData)
+    .then(({ data }) => data.data)
+    .catch((error) => Promise.reject(error));
+}
+
+export function toggleProjectStatusApi({ id, postData }: any): Promise<any> {
+  return http
+    .patch(`/project/${id}`, postData)
+    .then(({ data }) => data.data)
+    .catch((error) => Promise.reject(error));
+}
+
+export function getProjectApi(id: string | undefined): Promise<any> {
+  return http
+    .get(`/project/${id}`)
+    .then(({ data }) => data.data)
+    .catch((error) => Promise.reject(error));
+}
+
+export function getProjectsApi(qs:any): Promise<any> {
+  return http.get(`/project/list${qs}`).then(({ data }) => {
+    return data.data;
+  }).catch((error)=>Promise.reject(error))
+}
