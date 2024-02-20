4365036542a20d3447a015fd4272d3c4c6d943a3
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-20 21:54:57 +0330

    Subject: Dashboard, Description: Add new api calling, Date: 2/20/2024

--- next-folder/src/hooks/proposals/use-proposals.ts
+++ next-folder/src/hooks/proposals/use-proposals.ts
@@ -2,12 +2,12 @@ import { getProposalsApi } from "@/services/proposal-service";
 import { useQuery } from "@tanstack/react-query";
 
 export default function useProposals() {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["proposals"],
     queryFn: getProposalsApi,
   });
 
   const { proposals } = data || {};
 
-  return { proposals, isLoading };
+  return { proposals, isLoading, isError };
 }
