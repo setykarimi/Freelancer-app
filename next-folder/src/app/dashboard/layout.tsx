ebce322a6bbd4bc1daadb35b47c8c831b95af4f1
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-22 13:09:51 +0330

    Subject: Dashboard, Description: Create new parallel routes, Date: 2/22/2024

--- next-folder/src/app/dashboard/layout.tsx
+++ next-folder/src/app/dashboard/layout.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import Sidebar from "./_layout/sidebar";
 
 export default function DashboardLayout({
   children,
-}: Readonly<{
+  sidebar,
+}: {
+  sidebar: React.ReactNode;
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <div className="grid lg:grid-rows-[auto_1fr] lg:grid-cols-[15rem_1fr] h-screen w-full max-w-[1920px]">
       {/* <Navbar /> */}
-      <Sidebar />
+      {sidebar}
 
       <div className="bg-secondary-100 p-8 overflow-y-auto h-full">
         <div className="mx-auto max-w-screen-2xl flex flex-col gap-y-12">
