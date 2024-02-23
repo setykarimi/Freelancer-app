8838cac61f464753566d85189e1016726c301f95
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-23 13:20:13 +0330

    Subject: Dashboard, Description: Create new page for all projects, Date: 2/23/2024

---
+++ next-folder/src/app/dashboard/admin/all-projects/page.tsx
@@ -0,0 +1,10 @@
+import AllProjectsTable from "@/features/projects/all-projects/table";
+
+export default function AllProjects() {
+  return (
+    <>
+      <h1 className="font-black text-secondary-700 text-xl">پروژه‌ها</h1>
+      <AllProjectsTable />
+    </>
+  );
+}
