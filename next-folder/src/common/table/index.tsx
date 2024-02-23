436af5460686ba37e425a3d69fa0955a7c7c546f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-23 12:55:15 +0330

    Subject: Dashboard, Description: Create new folder for table, Date: 2/23/2024

---
+++ next-folder/src/common/table/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+// Compound component pattern
+export default function Table({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-secondary-0 overflow-x-auto">
+      <table>{children}</table>
+    </div>
+  );
+}
+
+function TableHeader({ children }: { children: React.ReactNode }) {
+  return (
+    <thead>
+      <tr className="title-row">{children}</tr>
+    </thead>
+  );
+}
+
+function TableBody({ children }: { children: React.ReactNode }) {
+  return <tbody>{children}</tbody>;
+}
+
+function TableRow({ children }: { children: React.ReactNode }) {
+  return <tr>{children}</tr>;
+}
+
+Table.Header = TableHeader;
+Table.Body = TableBody;
+Table.Row = TableRow;
