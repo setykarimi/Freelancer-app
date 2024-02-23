3e897dc038a3ce2460a6cd6a11abb7fe656124ac
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-23 13:18:45 +0330

    Subject: Dashboard, Description: Create new hook for proposals, Date: 2/23/2024

---
+++ next-folder/src/hooks/proposals/use-create-proposal.tsx
@@ -0,0 +1,22 @@
+import { createProposalApi } from "@/services/proposal-service";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+export default function useCreateProposal() {
+  const queryClient = useQueryClient();
+
+  const { isPending: isCreating, mutate: createProposal } = useMutation({
+    mutationFn: createProposalApi,
+    onSuccess: (data) => {
+      toast.success(data.message);
+      queryClient.invalidateQueries({
+        queryKey: ["proposals"],
+      });
+    },
+    onError: (error: any) => {
+      toast.error(error?.response?.data?.message);
+    },
+  });
+
+  return { isCreating, createProposal };
+}
