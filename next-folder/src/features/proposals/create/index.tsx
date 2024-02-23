1ea3c3565cedffeeeeb09c33fd153effd65ee91a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-23 13:19:16 +0330

    Subject: Dashboard, Description: Create new feature for proposals, Date: 2/23/2024

---
+++ next-folder/src/features/proposals/create/index.tsx
@@ -0,0 +1,97 @@
+import TextField from "@/common/form/text-field";
+import Loading from "@/common/loading";
+import useCreateProposal from "@/hooks/proposals/use-create-proposal";
+import { useForm } from "react-hook-form";
+
+export default function CreateProposal({
+  onClose,
+  projectId,
+}: {
+  onClose: () => void;
+  projectId: string;
+}) {
+  const { createProposal } = useCreateProposal();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (values: any) => {
+    console.log(values);
+    createProposal(
+      {
+        ...values,
+        projectId,
+      },
+      {
+        onSuccess: () => {
+          onClose();
+        },
+      }
+    );
+  };
+  return (
+    <div>
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          name="description"
+          label="توضیحات"
+          register={register}
+          required
+          validationSchema={{
+            required: {
+              value: true,
+              message: "توضیحات ضروری است",
+            },
+            minLength: {
+              value: 10,
+              message: "طول عنوان نامعتبر است",
+            },
+          }}
+          errors={errors}
+        />
+
+        <TextField
+          name="price"
+          label="قیمت"
+          register={register}
+          type="number"
+          required
+          validationSchema={{
+            required: {
+              value: true,
+              message: "قیمت ضروری است",
+            },
+          }}
+          errors={errors}
+        />
+
+        <TextField
+          name="duration"
+          label="مدت زمان"
+          register={register}
+          type="number"
+          required
+          validationSchema={{
+            required: {
+              value: true,
+              message: "مدت زمات ضروری است",
+            },
+          }}
+          errors={errors}
+        />
+
+        <div className="mt-8">
+          {false ? (
+            <Loading />
+          ) : (
+            <button className="btn btn--primary w-full" type="submit">
+              تایید
+            </button>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+}
