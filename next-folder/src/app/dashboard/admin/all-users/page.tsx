ab92bc12faf5e2212a9b70877af958f189ff9a27
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-23 12:54:30 +0330

    Subject: Dashboard, Description: Create new folder for all users, Date: 2/23/2024

---
+++ next-folder/src/app/dashboard/admin/all-users/page.tsx
@@ -0,0 +1,10 @@
+import AllUsersTable from "@/features/users/all-users/table";
+
+export default function AllUsers() {
+  return (
+    <>
+      <h1 className="font-black text-secondary-700 text-xl mb-8">کاربران</h1>
+      <AllUsersTable />
+    </>
+  );
+}
