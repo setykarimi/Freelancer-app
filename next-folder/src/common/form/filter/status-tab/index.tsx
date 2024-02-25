0e8f36fb9294b9a9973809c6c2dd995988300acd
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-25 13:34:55 +0330

    Subject: Dashboard, Description: Edit for send search params, Date: 2/25/2024

--- next-folder/src/common/form/filter/status-tab/index.tsx
+++ next-folder/src/common/form/filter/status-tab/index.tsx
@@ -1,43 +1,35 @@
-import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function StatusFilter({
-  filterField,
   options,
+  searchParams,
 }: {
-  filterField: string;
   options: { value: string; label: string }[];
+  searchParams: any;
 }) {
-  const router = useRouter();
-  const [currentFilter, setCurrentFilter] = useState<string>('');
-
-  useEffect(() => {
-    const queryValue:any = router.query[filterField];
-    setCurrentFilter(queryValue || options[0].value);
-  }, [router.query, filterField, options]);
-
-  function handleChange(value: string) {
-    router.push({ pathname: router.pathname, query: { ...router.query, [filterField]: value } });
-    setCurrentFilter(value);
-  }
+  const current_path = usePathname();
 
   return (
     <div className="flex items-center gap-x-2 text-xs">
       <span>وضعیت</span>
       <div className="flex items-center gap-x-2 border border-secondary-100 bg-secondary-0 rounded-lg p-1">
         {options.map(({ value, label }) => {
-          const isActive = value === currentFilter;
+          const isActive = value === searchParams?.status || options[0].value;
           return (
-            <button
-              key={value}
-              disabled={isActive}
-              onClick={() => handleChange(value)}
+            <Link
+              href={{
+                pathname: current_path,
+                query: { status: value },
+              }}
               className={`whitespace-nowrap rounded-md px-4 py-1 font-bold transition-all duration-300 text-xs ${
-                isActive ? 'bg-primary-900 text-white' : 'bg-secondary-0 text-secondary-800'
+                isActive
+                  ? "bg-primary-900 text-white"
+                  : "bg-secondary-0 text-secondary-800"
               }`}
             >
               {label}
-            </button>
+            </Link>
           );
         })}
       </div>
