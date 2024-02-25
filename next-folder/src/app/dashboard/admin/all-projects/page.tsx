bc25859c9f265dcbb72466c34dbd18c9a0eee851
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-25 13:34:41 +0330

    Subject: Dashboard, Description: Send search params, Date: 2/25/2024

--- next-folder/src/app/dashboard/admin/all-projects/page.tsx
+++ next-folder/src/app/dashboard/admin/all-projects/page.tsx
@@ -1,10 +1,10 @@
 import AllProjectsTable from "@/features/projects/all-projects/table";
 
-export default function AllProjects() {
+export default function AllProjects({ searchParams }: { searchParams: any }) {
   return (
     <>
       <h1 className="font-black text-secondary-700 text-xl">پروژه‌ها</h1>
-      <AllProjectsTable />
+      <AllProjectsTable searchParams={searchParams}/>
     </>
   );
 }
