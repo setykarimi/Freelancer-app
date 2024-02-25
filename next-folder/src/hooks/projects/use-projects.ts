961815f48bdbfc24cd0269532462d249483cc820
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-25 13:34:25 +0330

    Subject: Dashboard, Description: Send search params, Date: 2/25/2024

--- next-folder/src/hooks/projects/use-projects.ts
+++ next-folder/src/hooks/projects/use-projects.ts
@@ -1,15 +1,17 @@
 import { getProjectsApi } from "@/services/project-services";
 import { useQuery } from "@tanstack/react-query";
-import { useSearchParams } from "next/navigation";
 
 export default function useProjects() {
-  const search = useSearchParams();
+  const search = window.location.search
+  const queryObject = Object.fromEntries(
+    new URLSearchParams(search)
+  );
 
-  const queryObject = Object.fromEntries(new URLSearchParams(""));
+  console.log("queryObject", queryObject);
 
-  const { data, isLoading , isError} = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["projects", queryObject],
-    queryFn: () => getProjectsApi(""),
+    queryFn: () => getProjectsApi(search),
   });
 
   const { projects } = data || {};
