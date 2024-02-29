889a63be8afaf90c4513f96790a34e6f0c4f009c
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-29 16:32:53 +0330

    Subject: Owner, Description: Create new hook for owner projects, Date: 2/29/2024

---
+++ next-folder/src/hooks/projects/use-owner-projects.ts
@@ -0,0 +1,13 @@
+import { getOwnerProjectsApi } from "@/services/project-services";
+import { useQuery } from "@tanstack/react-query";
+
+export default function useOwnerProjects() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["owner-projects"],
+    queryFn: getOwnerProjectsApi,
+  });
+
+  const { projects } = data || {};
+
+  return { isLoading, projects };
+}
