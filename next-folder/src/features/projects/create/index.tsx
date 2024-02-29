5df9ba9eaa2e03105515921dd80f2c6124722689
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-29 19:30:36 +0330

    Subject: Owner, Description: Create new folder owner project, Date: 2/29/2024

---
+++ next-folder/src/features/projects/create/index.tsx
@@ -0,0 +1,145 @@
+import DatePickerField from "@/common/form/date-picker";
+import RHFSelect from "@/common/form/rhf-select";
+import TextField from "@/common/form/text-field";
+import Loading from "@/common/loading";
+import useCategories from "@/hooks/category/use-categories";
+import useCreateProject from "@/hooks/projects/use-create-project";
+import useEditProject from "@/hooks/projects/use-edit-project";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { TagsInput } from "react-tag-input-component";
+
+function CreateProjectForm({
+  onClose,
+  projectToEdit = {},
+}: {
+  onClose: any;
+  projectToEdit?: any;
+}) {
+  const { _id: editId } = projectToEdit;
+  const isEditSession = Boolean(editId);
+
+  const {
+    title,
+    description,
+    budget,
+    category,
+    deadline,
+    tags: prevTags,
+  } = projectToEdit;
+  let editValues = {};
+  if (isEditSession) {
+    editValues = {
+      title,
+      description,
+      budget,
+      category: category._id,
+    };
+  }
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm({ defaultValues: editValues });
+
+  const [tags, setTags] = useState(prevTags || []);
+  const [date, setDate] = useState(new Date(deadline || ""));
+  const { categories } = useCategories();
+  const { isCreating, createProject } = useCreateProject();
+  const { editProject, isEditing } = useEditProject();
+
+  const onSubmit = (data: any) => {
+    const newProject = {
+      ...data,
+      deadline: new Date(date).toISOString(),
+      tags,
+    };
+
+    if (isEditSession) {
+      editProject(
+        { id: editId, newProject },
+        {
+          onSuccess: () => {
+            onClose();
+            reset();
+          },
+        }
+      );
+    } else {
+      createProject(newProject, {
+        onSuccess: () => {
+          onClose();
+          reset();
+        },
+      });
+    }
+  };
+
+  return (
+    <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        label="عنوان"
+        name="title"
+        register={register}
+        required
+        validationSchema={{
+          required: "عنوان ضروری است",
+          minLength: {
+            value: 10,
+            message: "حداقل 10 کاراکتر را وارد کنید",
+          },
+        }}
+        errors={errors}
+      />
+      <TextField
+        label="توضیحات"
+        name="description"
+        register={register}
+        required
+        validationSchema={{
+          required: "توضیحات ضروری است",
+          minLength: {
+            value: 15,
+            message: "حداقل 15 کاراکتر را وارد کنید",
+          },
+        }}
+        errors={errors}
+      />
+      <TextField
+        label="بودجه"
+        name="budget"
+        type="number"
+        register={register}
+        required
+        validationSchema={{
+          required: "بودجه ضروری است",
+        }}
+        errors={errors}
+      />
+      <RHFSelect
+        label="دسته بندی"
+        required
+        name="category"
+        register={register}
+        options={categories}
+      />
+      <div>
+        <label className="mb-2 block text-secondary-700">تگ</label>
+        <TagsInput value={tags} onChange={setTags} name="tags" />
+      </div>
+      <DatePickerField date={date} setDate={setDate} label="ددلاین" />
+      <div className="!mt-8">
+        {isCreating || isEditing ? (
+          <Loading />
+        ) : (
+          <button type="submit" className="btn btn--primary w-full">
+            تایید
+          </button>
+        )}
+      </div>
+    </form>
+  );
+}
+export default CreateProjectForm;
