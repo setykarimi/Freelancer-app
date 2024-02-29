8e7e31f07dff45097d8e5634c6a6b52ce0839081
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-29 19:30:01 +0330

    Subject: Owner, Description: Create new folder for proposals component, Date: 2/29/2024

---
+++ next-folder/src/features/proposals/components/change-proposal-status.tsx
@@ -0,0 +1,77 @@
+import RHFSelect from "@/common/form/rhf-select";
+import Loading from "@/common/loading";
+import useChangeProposalStatus from "@/hooks/proposals/use-change-proposal-status";
+import { useQueryClient } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { useForm } from "react-hook-form";
+
+
+const options = [
+  {
+    label: "رد شده",
+    value: 0,
+  },
+  {
+    label: "در انتظار تایید",
+    value: 1,
+  },
+  {
+    label: "تایید شده",
+    value: 2,
+  },
+];
+
+export default function ChangeProposalStatus({
+  proposalId,
+  onClose,
+}: {
+  proposalId: string;
+  onClose: () => void;
+}) {
+    const {id} = useParams()
+  const { register, handleSubmit } = useForm();
+
+  const { changeUserStatus, isEditing } = useChangeProposalStatus();
+  const queryClient = useQueryClient();
+
+  const onSubmit = (values: any) => {
+    console.log(values);
+    changeUserStatus(
+      {
+        id: proposalId,
+        postData: {
+            status: +values.status
+        },
+      },
+      {
+        onSuccess: () => {
+          onClose();
+          queryClient.invalidateQueries({queryKey:["project", id]})
+        },
+      }
+    );
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <RHFSelect
+          register={register}
+          label="تغییر وضعیت"
+          options={options}
+          name="status"
+          required
+        />
+
+        <div className="!mt-8">
+          {isEditing ? (
+            <Loading />
+          ) : (
+            <button className="btn btn--primary w-full" type="submit">
+              تایید
+            </button>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+}
