97291110f31e8d2778627daadd5ef0d08862662c
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-29 15:53:44 +0330

    Subject: Proposals, Description: Create new page, Date: 2/29/2024

---
+++ next-folder/src/app/dashboard/admin/all-proposals/page.tsx
@@ -0,0 +1,10 @@
+import AllProposalsTable from "@/features/proposals/table";
+
+export default function AllProposalsPage() {
+  return (
+    <>
+      <h1 className="font-black text-secondary-700 text-xl">درخواست‌ها</h1>
+      <AllProposalsTable />
+    </>
+  );
+}
