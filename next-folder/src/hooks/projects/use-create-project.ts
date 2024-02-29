44ffedafe059ef2c03f6ad05ef53ac5960f01815
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-02-29 19:29:13 +0330

    Subject: Owner, Description: Create new hook for create project, Date: 2/29/2024

---
+++ next-folder/src/hooks/projects/use-create-project.ts
@@ -0,0 +1,22 @@
+import { createProjectApi } from "@/services/project-services";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+
+export default function useCreateProject() {
+  const queryClient = useQueryClient();
+
+  const { isPending: isCreating, mutate: createProject } = useMutation({
+    mutationFn: createProjectApi,
+    onSuccess: (data) => {
+      toast.success(data.message);
+
+      queryClient.invalidateQueries({
+        queryKey: ["owner-projects"],
+      });
+    },
+
+    onError: (err: any) => toast.error(err?.response?.data?.message),
+  });
+
+  return { isCreating, createProject };
+}
