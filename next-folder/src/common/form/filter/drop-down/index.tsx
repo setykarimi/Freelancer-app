d8140bee42a4e8e58fe90710fa353c71b5d0946f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-04 22:42:46 +0330

    Subject: Dashboard, Description: Edit for dropdown filter, Date: 3/4/2024

--- next-folder/src/common/form/filter/drop-down/index.tsx
+++ next-folder/src/common/form/filter/drop-down/index.tsx
@@ -1,34 +1,45 @@
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function FilterDropDown({
   options,
   filterField,
-  searchParams
+  searchParams,
 }: {
   options: any[];
   filterField: any;
-  searchParams: any
+  searchParams: any;
 }) {
-  
   const current_path = usePathname();
   const router = useRouter();
-  
-  const filterValue = searchParams[filterField]
-  
+
+  const filterValue = searchParams[filterField];
+  const { status, category, sort } = searchParams;
+
   function handleChange(e: any) {
-    
-    router.push(`${current_path}?status=${searchParams?.status}&${filterField}=${e.target.value}`);
+    let name = e.target.name;
+    if (name == "sort") {
+      router.push(
+        `${current_path}?status=${
+          status !== undefined ? status : ""
+        }&category=${category !== undefined ? category : ""}&sort=${
+          e.target.value
+        }`
+      );
+    } else if (name == "category") {
+      router.push(
+        `${current_path}?status=${
+          status !== undefined ? status : ""
+        }&category=${e.target.value}&sort=${sort !== undefined ? sort : ""}`
+      );
+    }
   }
-
-  
-
-  
 
   return (
     <select
       value={filterValue}
       onChange={handleChange}
       className="textField__input py-2 text-xs bg-secondary-0"
+      name={filterField}
     >
       {options.map((item) => (
         <option key={item.value} value={item.value}>
