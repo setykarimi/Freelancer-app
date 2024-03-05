46bf8766921865cb0eec8e731f22f9517e77dfec
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-05 21:29:42 +0330

    Subject: Dark mode, Description: Create new feature for theme, Date: 3/5/2024

---
+++ next-folder/src/features/theme/index.tsx
@@ -0,0 +1,15 @@
+import { useDarkMode } from "@/context/dark-mode";
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi";
+
+export default function DarkModeToggle() {
+  const { isDarkMode, toggleDarkMode }: any = useDarkMode();
+  return (
+    <button onClick={toggleDarkMode}>
+      {isDarkMode ? (
+        <HiOutlineSun className="w-5 h-5 text-secondary-500" />
+      ) : (
+        <HiOutlineMoon className="w-5 h-5 text-secondary-500" />
+      )}
+    </button>
+  );
+}
