52b0ae595719d9efac01059c368cf0ed772ad80f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-05 21:29:02 +0330

    Subject: Dark mode, Description: Create new hook for use local storage, Date: 3/5/2024

---
+++ next-folder/src/hooks/other/use-local-storage.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export default function useLocalStorageState(key: string, initalState: any) {
+  const storedValue = localStorage.getItem(key) || null;
+  const [value, setValue] = useState(
+    storedValue ? JSON.parse(storedValue) : initalState
+  );
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+}
