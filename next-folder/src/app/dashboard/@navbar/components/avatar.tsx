3474c590e23d7fd4f6c884c141c0910a46f5de5f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-10 22:18:33 +0330

    Subject: Dashboard, Description: Edit icon, Date: 3/10/2024

--- next-folder/src/app/dashboard/@navbar/components/avatar.tsx
+++ next-folder/src/app/dashboard/@navbar/components/avatar.tsx
@@ -1,15 +1,12 @@
 import useUser from "@/hooks/authentication/use-user";
+import { IoPersonSharp } from "react-icons/io5";
 
 export default function Avatar() {
   const { user } = useUser();
 
   return (
     <div className="flex items-center gap-x-2">
-      <img
-        className="w-7 h-7 rounded-full object-cover object-left"
-        src="/user.jpg"
-        alt="user-account"
-      />
+      <IoPersonSharp color="#fff" />
       <span className="text-secondary-700 font-bold text-sm">
         {user?.name} عزیز، خوش آمدید
       </span>
