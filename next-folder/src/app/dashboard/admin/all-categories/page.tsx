0f3df0c9c2b575a87c304af1e122ec86007a281b
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-12 22:11:18 +0330

    Subject: Categories, Description: Add title and modal for create new category, Date: 3/12/2024

--- next-folder/src/app/dashboard/admin/all-categories/page.tsx
+++ next-folder/src/app/dashboard/admin/all-categories/page.tsx
@@ -1,10 +1,32 @@
+"use client"
+import Modal from "@/common/modal";
 import AllCategoriesTable from "@/features/categories/all-categories/table";
-import React from "react";
+import CreateCategoryForm from "@/features/categories/create";
+import { useState } from "react";
+import { HiOutlinePlus } from "react-icons/hi";
 
 export default function page() {
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <h1 className="font-black text-secondary-700 text-xl">دسته‌بندی‌ها</h1>
+      <div className="flex items-center justify-between mb-8">
+        <h1 className="font-black text-secondary-700 text-xl">دسته‌بندی‌ها</h1>
+        <Modal
+          title="اضافه کردن دسته‌بندی جدید"
+          open={open}
+          onClose={() => setOpen(false)}
+        >
+          <CreateCategoryForm onClose={() => setOpen(false)} />
+        </Modal>
+        <button
+          onClick={() => setOpen(true)}
+          className="btn btn--primary flex items-center gap-x-2"
+        >
+          <HiOutlinePlus />
+          <span>اضافه کردن دسته‌بندی</span>
+        </button>
+      </div>
+
       <AllCategoriesTable />
     </>
   );
