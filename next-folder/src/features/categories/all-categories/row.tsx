d0bf67ea74deca5f8d604969756b93d13f4494af
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-13 23:50:36 +0330

    Subject: Categories, Description: Add edit and delete function, Date: 3/13/2024

--- next-folder/src/features/categories/all-categories/row.tsx
+++ next-folder/src/features/categories/all-categories/row.tsx
@@ -1,5 +1,14 @@
+import Modal from "@/common/modal";
 import Table from "@/common/table";
+import CreateProjectForm from "@/features/projects/create";
+import ConfirmDelete from "@/features/projects/owner-projects/table/components/confirm-delete";
+import useRemoveCategory from "@/hooks/category/use-remove-category";
+import useRemoveProject from "@/hooks/projects/use-remove-project";
 import toLocalDateShort from "@/utils/to-local-date-short";
+import { useState } from "react";
+import { HiOutlineTrash } from "react-icons/hi";
+import { TbPencilMinus } from "react-icons/tb";
+import CreateCategoryForm from "../create";
 
 export default function AllCategoriesTableRow({
   category,
@@ -8,6 +17,10 @@ export default function AllCategoriesTableRow({
   category: any;
   index: number;
 }) {
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  const [isDeleteOpen, setIsDelteOpen] = useState(false);
+  const { removeCategory } = useRemoveCategory();
+
   return (
     <Table.Row>
       <td>{++index}</td>
@@ -17,7 +30,43 @@ export default function AllCategoriesTableRow({
       <td>{category.description}</td>
       <td>{toLocalDateShort(category.createdAt)}</td>
       <td>{toLocalDateShort(category.updatedAt)}</td>
-      <td></td>
+      <td>
+      <div className="flex items-center gap-x-4">
+            <>
+              <button onClick={() => setIsEditOpen(true)}>
+                <TbPencilMinus className="w-5 h-5 text-primary-900" />
+              </button>
+              <Modal
+                open={isEditOpen}
+                title={`ویرایش دسته بندی ${category.title}`}
+                onClose={() => setIsEditOpen(false)}
+              >
+                <CreateCategoryForm onClose={()=> setIsEditOpen(false)} categoryToEdit={category}/>
+              </Modal>
+            </>
+            <button onClick={() => setIsDelteOpen(true)}>
+              <HiOutlineTrash className="w-5 h-5 text-error" />
+            </button>
+            <Modal
+              open={isDeleteOpen}
+              title={`حذف ${category.title}`}
+              onClose={() => setIsDelteOpen(false)}
+            >
+              <ConfirmDelete
+                onClose={() => {
+                  setIsDelteOpen(false);
+                }}
+                onConfirm={() =>
+                  removeCategory(category._id, {
+                    onSuccess: () => setIsDelteOpen(false),
+                  })
+                }
+                resourceName={category.title}
+                disabled={false}
+              />
+            </Modal>
+          </div>
+      </td>
     </Table.Row>
   );
 }
