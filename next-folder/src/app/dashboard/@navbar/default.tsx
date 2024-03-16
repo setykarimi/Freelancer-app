803c85aa54174597cfd4cefd05c0b715efe2c942
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-16 22:17:45 +0330

    Subject: Dashboard, Description: Edit classNames, Date: 3/16/2024

--- next-folder/src/app/dashboard/@navbar/default.tsx
+++ next-folder/src/app/dashboard/@navbar/default.tsx
@@ -3,10 +3,10 @@ import useUser from "@/hooks/authentication/use-user";
 import Avatar from "./components/avatar";
 
 export default function Navbar() {
-  const { isLoading, user } = useUser();
+  const { isLoading } = useUser();
 
   return (
-    <div className="bg-secondary-0 py-3 px-8 border-b border-b-secondary-200">
+    <div className="bg-secondary-0 py-3 px-4 border-b border-b-secondary-200">
       <div
         className={`container xl:max-w-screen-lg flex items-center justify-end gap-x-4 ${
           isLoading ? "blur-sm opacity-50" : ""
