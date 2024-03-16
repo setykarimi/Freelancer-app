7c7a51b83db5c27fdce0af9cbf71b58b47a68802
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-16 22:16:57 +0330

    Subject: Dashboard, Description: Edit cl, Date: 3/16/2024

--- next-folder/src/features/theme/index.tsx
+++ next-folder/src/features/theme/index.tsx
@@ -6,15 +6,15 @@ export default function DarkModeToggle() {
   return (
     <button onClick={toggleDarkMode}>
       {isDarkMode ? (
-     <div>
-         <HiOutlineSun className="w-5 h-5 text-secondary-500" />
-         تغییر به روز
-     </div>
+        <div className="flex items-center gap-2">
+          <HiOutlineSun size={13} className="text-secondary-500" />
+          <span className="text-xs"> تغییر به روز</span>
+        </div>
       ) : (
-      <div>
-          <HiOutlineMoon className="w-5 h-5 text-secondary-500" />
-          تغییر به شب
-      </div>
+        <div className="flex items-center gap-2">
+          <HiOutlineMoon size={13} className="text-secondary-500" />
+          <span className="text-xs">تغییر به شب</span>
+        </div>
       )}
     </button>
   );
