dd13f9d3f842714d5a3e693c7ccca2c349208d8d
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-19 23:16:49 +0330

    Subject: Dashboard, Description: Edit style for stats, Date: 3/19/2024

--- next-folder/src/common/stats/index.tsx
+++ next-folder/src/common/stats/index.tsx
@@ -1,30 +1,37 @@
+import truncateText from "@/utils/truncate-text";
 import React from "react";
 
 const colors = {
   primary: "bg-primary-100 text-primary-700",
-  green: "bg-green-100 text-green-700",
-  yellow: "bg-yellow-100 text-yellow-700",
+  blue: "bg-blue-100 text-blue-700",
+  orange: "bg-orange-100 text-orange-500",
+  red: "bg-red-100 text-red-500",
 };
 
 type StateType = {
   icon: React.ReactNode;
-  value: number | string;
+  value: string;
   title: string;
-  color: "primary" | "green" | "yellow";
+  color: "primary" | "blue" | "orange" | "red";
 };
 
 export default function Stat({ icon, value, title, color }: StateType) {
   return (
-    <div className="grid grid-rows-2 grid-cols-[6.4rem_1fr] bg-secondary-0 p-4 rounded-lg gap-x-4 ">
-      <div
-        className={`row-span-2 flex items-center justify-center p-2 aspect-square rounded-full ${colors[color]}`}
-      >
-        {icon}
+    <div className="bg-secondary-0 p-2 rounded-md">
+      <div className="flex  gap-2 items-center">
+        <div
+          className={`w-10 h-10 flex items-center justify-center p-2 aspect-square rounded-md ${colors[color]}`}
+        >
+          {icon}
+        </div>
+        <div>
+          <h5 className="font-bold text-lg text-secondary-500">{title}</h5>
+          <p className="text-secondary-400 text-xs mt-1">{value}</p>
+        </div>
       </div>
-      <h5 className="font-bold text-lg text-secondary-500 self-center">
-        {title}
-      </h5>
-      <p className="text-3xl font-bold text-secondary-900">{value}</p>
+      <div className="mt-3 flex justify-between">
+        <span className="badge  badge--secondary block text-xs w-fit mr-auto">+{truncateText(value, 10)}</span>
+      </div>
     </div>
   );
 }
