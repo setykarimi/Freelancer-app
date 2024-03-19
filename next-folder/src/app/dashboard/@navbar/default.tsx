400ffc18e553692442d6b7a69f104913a7a8cf8c
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-19 16:19:43 +0330

    Subject: Dashboard, Description: Edit style, Date: 3/19/2024

--- next-folder/src/app/dashboard/@navbar/default.tsx
+++ next-folder/src/app/dashboard/@navbar/default.tsx
@@ -6,9 +6,9 @@ export default function Navbar() {
   const { isLoading } = useUser();
 
   return (
-    <div className="bg-secondary-0 py-3 px-4 border-b border-b-secondary-200">
+    <div className="bg-secondary-0 py-3 border-b border-b-secondary-200">
       <div
-        className={`container xl:max-w-screen-lg flex items-center justify-end gap-x-4 ${
+        className={`container px-4 xl:max-w-screen-lg flex items-center justify-end gap-x-4 ${
           isLoading ? "blur-sm opacity-50" : ""
         }`}
       >
