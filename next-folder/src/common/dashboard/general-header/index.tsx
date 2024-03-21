a624738e4e58bd66dde03bb4eab2f6bef766edd7
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-21 23:27:16 +0330

    Subject: Dashboard, Description: Edit style, Date: 3/21/2024

--- next-folder/src/common/dashboard/general-header/index.tsx
+++ next-folder/src/common/dashboard/general-header/index.tsx
@@ -1,8 +1,8 @@
 export default function GeneralHeader() {
     return (
       <div>
-        <h1 className="font-black text-2xl text-secondary-900 mb-4">آمار کلی</h1>
-        <p className="text-secondary-600">
+        <h1 className="font-black text-2xl text-secondary-800 mb-2">آمار کلی</h1>
+        <p className="text-secondary-500">
           در یک نما خلاصه‌ای از آمار خود را ببینید
         </p>
       </div>
