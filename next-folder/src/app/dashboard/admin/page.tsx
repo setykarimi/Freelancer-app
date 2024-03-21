5feda450cea9c58c566e53c0e5d9460d99c48e4e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-21 23:26:34 +0330

    Subject: Dashboard, Description: Edit for showing stats, Date: 3/21/2024

--- next-folder/src/app/dashboard/admin/page.tsx
+++ next-folder/src/app/dashboard/admin/page.tsx
@@ -1,77 +1,23 @@
 "use client";
 import GeneralHeader from "@/common/dashboard/general-header";
 import Loading from "@/common/loading";
-import Stat from "@/common/stats";
+import CategoriesStats from "@/features/dashboard/admin/categories-stats";
+import ProjectStats from "@/features/dashboard/admin/projects-stats";
+import ProposalStats from "@/features/dashboard/admin/proposals-stats";
+import UserStats from "@/features/dashboard/admin/user-stats";
 import useUsers from "@/hooks/authentication/use-users";
-import useCategories from "@/hooks/category/use-categories";
-import useProjects from "@/hooks/projects/use-projects";
-import useProposals from "@/hooks/proposals/use-proposals";
-import { toPersianNumbers } from "@/utils/to-persian-numbers";
-import {
-  BiSolidCategory,
-  BiSolidDuplicate,
-  BiSolidLayer,
-} from "react-icons/bi";
-import { PiUsersThree } from "react-icons/pi";
 
 export default function AdminMainPage() {
-  const {
-    isLoading: isLoading1,
-    proposals,
-    isError: error1,
-  }: any = useProposals();
-
-  const {
-    isLoading: isLoading2,
-    projects,
-    isError: error2,
-  }: any = useProjects();
-
-  const { isLoading: isLoading3, users, isError: error3 }: any = useUsers();
-
-  const {
-    isLoading: isLoading4,
-    rawCategories,
-    isError: error4,
-  }: any = useCategories();
-
-  if (isLoading1 || isLoading2 || isLoading3 || isLoading4) return <Loading />;
-
-  if (error1 || error2 || error3)
-    throw new Error(
-      error1 ? error1 : error2 ? error2 : error3 ? error3 : error4
-    );
+  
 
   return (
     <>
       <GeneralHeader />
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4">
-        <Stat
-          color="orange"
-          title="کاربران"
-          value={`${toPersianNumbers(users?.length)} کاربر فعال`}
-          icon={<PiUsersThree className="lg:w-20 w-16 lg:h-20 h-16" />}
-        />
-        <Stat
-          color="primary"
-          title="درخواست‌ها"
-          value={`${toPersianNumbers(proposals?.length)} درخواست فعال`}
-          icon={<BiSolidLayer className="lg:w-20 w-16 lg:h-20 h-16" />}
-        />
-        <Stat
-          color="red"
-          title="پروژه‌ها"
-          value={`${toPersianNumbers(projects?.length)} پروژه فعال`}
-          icon={<BiSolidDuplicate className="lg:w-20 w-16 lg:h-20 h-16" />}
-        />
-        <Stat
-          color="blue"
-          title="دسته‌بندی‌ها"
-          value={`${toPersianNumbers(
-            rawCategories?.length
-          )} دسته‌‌بندی ثبت شده`}
-          icon={<BiSolidCategory className="lg:w-20 w-16 lg:h-20 h-16" />}
-        />
+      <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+        <ProposalStats />
+        <UserStats />
+        <ProjectStats />
+        <CategoriesStats />
       </div>
     </>
   );
