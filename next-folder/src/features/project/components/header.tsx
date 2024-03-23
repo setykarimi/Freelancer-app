f151c6988a7add1b8616cc3d8366f17182e4c5f7
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-23 16:02:39 +0330

    Subject: Redesign, Description: Add data-title to table - edit classNames, Date: 3/23/2024

--- next-folder/src/features/project/components/header.tsx
+++ next-folder/src/features/project/components/header.tsx
@@ -4,7 +4,7 @@ import { HiArrowRight } from "react-icons/hi";
 export default function ProjectHeader({ project }: { project: any }) {
   const moveBack = useMoveBack();
   return (
-    <div className="flex items-center gap-x-4 mb-8">
+    <div className="flex items-center gap-x-4">
       <button onClick={moveBack}>
         <HiArrowRight className="w-5 h-5 text-secondary-500" />
       </button>
