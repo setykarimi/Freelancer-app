5c678c83bd9a14c2160328361ab3f1d8ee730a11
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-23 16:01:47 +0330

    Subject: Redesign, Description: Add data-title to table - edit classNames, Date: 3/23/2024

--- next-folder/src/app/dashboard/admin/all-projects/page.tsx
+++ next-folder/src/app/dashboard/admin/all-projects/page.tsx
@@ -2,9 +2,7 @@ import AllProjectsTable from "@/features/projects/all-projects/table";
 
 export default function AllProjects({ searchParams }: { searchParams: any }) {
   return (
-    <>
-      <h1 className="font-black text-secondary-700 text-xl">پروژه‌ها</h1>
+  
       <AllProjectsTable searchParams={searchParams}/>
-    </>
   );
 }
