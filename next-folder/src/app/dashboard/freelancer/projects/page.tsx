dffa7ec67133ee9e20ac598ca34ccb48116c6463
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-23 16:02:01 +0330

    Subject: Redesign, Description: Add data-title to table - edit classNames, Date: 3/23/2024

--- next-folder/src/app/dashboard/freelancer/projects/page.tsx
+++ next-folder/src/app/dashboard/freelancer/projects/page.tsx
@@ -1,10 +1,9 @@
 import AllProjectsTable from "@/features/projects/all-projects/table";
 
-export default function FreelancerProjects({ searchParams }: { searchParams: any }) {
-  return (
-    <>
-      <h1 className="font-black text-secondary-700 text-xl">پروژه‌ها</h1>
-      <AllProjectsTable searchParams={searchParams}/>
-    </>
-  );
+export default function FreelancerProjects({
+  searchParams,
+}: {
+  searchParams: any;
+}) {
+  return <AllProjectsTable searchParams={searchParams} />;
 }
