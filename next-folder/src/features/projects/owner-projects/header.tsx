9e5493e9a66635c5024fa147b74f6d6d4bb314dc
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-23 16:01:18 +0330

    Subject: Redesign, Description: Add data-title to table - edit classNames, Date: 3/23/2024

--- next-folder/src/features/projects/owner-projects/header.tsx
+++ next-folder/src/features/projects/owner-projects/header.tsx
@@ -8,7 +8,7 @@ import CreateProjectForm from "../create";
 function ProjectsHeader() {
   const [open, setOpen] = useState(false);
   return (
-    <div className="flex items-center justify-between mb-8">
+    <div className="flex items-center justify-between">
       <h1 className="font-black text-secondary-700 text-xl">پروژه های شما</h1>
       <Modal
         title="اضافه کردن پروژه جدید"
