ac2304dfa3e5d35cdac7dd178ce039ad1a3c2f10
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-23 16:01:12 +0330

    Subject: Redesign, Description: Add data-title to table - edit classNames, Date: 3/23/2024

--- next-folder/src/features/projects/owner-projects/table/row.tsx
+++ next-folder/src/features/projects/owner-projects/table/row.tsx
@@ -1,18 +1,17 @@
-"use client"
+"use client";
 import Modal from "@/common/modal";
 import Table from "@/common/table";
+import useRemoveProject from "@/hooks/projects/use-remove-project";
 import toLocalDateShort from "@/utils/to-local-date-short";
 import { toPersianNumbersWithComma } from "@/utils/to-persian-numbers";
 import truncateText from "@/utils/truncate-text";
+import Link from "next/link";
 import { useState } from "react";
+import { HiEye, HiOutlineTrash } from "react-icons/hi";
 import { TbPencilMinus } from "react-icons/tb";
 import CreateProjectForm from "../../create";
-import { HiEye, HiOutlineTrash } from "react-icons/hi";
-import Link from "next/link";
-import useRemoveProject from "@/hooks/projects/use-remove-project";
-import ToggleProjectStatus from "./components/toggle-project-status";
 import ConfirmDelete from "./components/confirm-delete";
-
+import ToggleProjectStatus from "./components/toggle-project-status";
 
 export default function ProjectRow({
   project,
@@ -29,11 +28,11 @@ export default function ProjectRow({
     <>
       <Table.Row key={project.id}>
         <td>{index + 1}</td>
-        <td>{truncateText(project.title, 30)}</td>
-        <td>{project.category.title}</td>
-        <td>{toPersianNumbersWithComma(project.budget)}</td>
-        <td>{toLocalDateShort(project.deadline)}</td>
-        <td>
+        <td data-title="عنوان پروژه">{truncateText(project.title, 30)}</td>
+        <td data-title="دسته‌بندی">{project.category.title}</td>
+        <td data-title="بودجه">{toPersianNumbersWithComma(project.budget)}</td>
+        <td data-title="ددلاین">{toLocalDateShort(project.deadline)}</td>
+        <td data-title="تگ‌ها">
           <div className="flex flex-wrap items-center gap-2 max-w-[200px]">
             {project.tags.map((tag: string) => (
               <span className="badge badge--secondary" key={tag}>
@@ -42,11 +41,11 @@ export default function ProjectRow({
             ))}
           </div>
         </td>
-        <td>{project.freelancer?.name || "-"}</td>
-        <td>
-          <ToggleProjectStatus project={project}/>
+        <td data-title="فریلنسر">{project.freelancer?.name || "-"}</td>
+        <td data-title="وضعیت">
+          <ToggleProjectStatus project={project} />
         </td>
-        <td>
+        <td data-title="عملیات">
           <div className="flex items-center gap-x-4">
             <>
               <button onClick={() => setIsEditOpen(true)}>
@@ -57,7 +56,10 @@ export default function ProjectRow({
                 title={`ویرایش ${project.title}`}
                 onClose={() => setIsEditOpen(false)}
               >
-                <CreateProjectForm onClose={()=> setIsEditOpen(false)} projectToEdit={project}/>
+                <CreateProjectForm
+                  onClose={() => setIsEditOpen(false)}
+                  projectToEdit={project}
+                />
               </Modal>
             </>
             <button onClick={() => setIsDelteOpen(true)}>
@@ -83,9 +85,12 @@ export default function ProjectRow({
             </Modal>
           </div>
         </td>
-        <td>
-          <Link href={`/dashboard/owner/project/${project._id}`} className="flex justify-center items-center">
-            <HiEye className="text-primary-800 w-5 h-5"/>
+        <td data-title="درخواست‌ها">
+          <Link
+            href={`/dashboard/owner/project/${project._id}`}
+            className="flex justify-center items-center"
+          >
+            <HiEye className="text-primary-800 w-5 h-5" />
           </Link>
         </td>
       </Table.Row>
