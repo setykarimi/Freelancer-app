754f92b884720f444b828a52f9f08a4851400ad6
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-25 20:12:49 +0330

    Subject: Redesing, Description: Edit for resend code, Date: 3/25/2024

--- next-folder/src/app/auth/page.tsx
+++ next-folder/src/app/auth/page.tsx
@@ -3,17 +3,19 @@ import CheckOTPForm from "@/features/authentication/check-otp";
 import SendOTPForm from "@/features/authentication/send-otp";
 import useUser from "@/hooks/authentication/use-user";
 import { getOtp } from "@/services/auth-service";
+import { convertPersianNumToEnglish } from "@/utils/convert-nums-to-english";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
+const RESEND_TIME = 90;
 
 export default function AuthPage() {
   const [step, setStep] = useState(1);
   const { user } = useUser();
   const router = useRouter();
-
+  const [time, setTime] = useState(RESEND_TIME);
   // useEffect(() => {
   //   if (user) router.replace("/");
   // }, [user]);
@@ -28,13 +30,16 @@ export default function AuthPage() {
 
   const sendOtpHandler = async (values: any) => {
     try {
-      const data = await mutateAsync(values);
+      const data = await mutateAsync({
+        phoneNumber: convertPersianNumToEnglish(values.phoneNumber.toString()),
+      });
       setStep(2);
       toast.success(data?.message);
     } catch (error: any) {
       const err_msg = error.response.data.message;
       toast.error(err_msg ? err_msg : "متاسفانه خطایی رخ داده است.");
     }
+    setTime(90);
   };
 
   const {
@@ -59,9 +64,11 @@ export default function AuthPage() {
         return (
           <CheckOTPForm
             onBack={() => setStep((prev) => prev - 1)}
-            onResendOtp={sendOtpHandler}
+            onResendOtp={handleSubmit(sendOtpHandler)}
             otpResponse={otpResponse}
             phoneNumber={getValues("phoneNumber")}
+            time={time}
+            setTime={setTime}
           />
         );
       default:
