0c7b8a6ae4c7867731e67ac269f856e8555c8c23
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-29 02:21:24 +0330

    Subject: Debug, Description: Edit for return error message, Date: 3/29/2024

--- next-folder/src/app/dashboard/freelancer/proposals/page.tsx
+++ next-folder/src/app/dashboard/freelancer/proposals/page.tsx
@@ -1,10 +1,5 @@
 import AllProposalsTable from "@/features/proposals/table";
 
 export default function Proposals() {
-  return (
-    <>
-      <h1 className="font-black text-secondary-700 text-xl">درخواست‌ها</h1>
-      <AllProposalsTable />
-    </>
-  );
+  return <AllProposalsTable />;
 }
