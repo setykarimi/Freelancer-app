20e66c6f36f98b1eb86c7c7567e766c308b43458
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-29 02:20:37 +0330

    Subject: Debug, Description: Edit for return error message, Date: 3/29/2024

--- next-folder/src/features/proposals/table/index.tsx
+++ next-folder/src/features/proposals/table/index.tsx
@@ -1,33 +1,37 @@
-"use client"
-import Loading from '@/common/loading';
-import Table from '@/common/table';
-import useProposals from '@/hooks/proposals/use-proposals';
-import React from 'react'
-import ProposalRow from './row';
+"use client";
+import Loading from "@/common/loading";
+import Table from "@/common/table";
+import useProposals from "@/hooks/proposals/use-proposals";
+import ProposalRow from "./row";
 
 export default function AllProposalsTable() {
-    const {isLoading, proposals} = useProposals()
-    if (isLoading) return <Loading />;
- 
-    if (!proposals.length) return <h3>یافت نشد</h3>;
-  
-    return (
-      <>
-        <Table>
-          <Table.Header>
-            <th>#</th>
-            <th>توضیحات</th>
-            <th>زمان تحویل</th>
-            <th>هزینه</th>
-            <th>وضعیت</th>
-            <th></th>
-          </Table.Header>
-          <Table.Body>
-            {proposals.map((proposal: any, index: number) => (
-              <ProposalRow key={proposal._id} proposal={proposal} index={index} />
-            ))}
-          </Table.Body>
-        </Table>
-      </>
-    );
+  const { isLoading, proposals, error }: any = useProposals();
+  if (isLoading) return <Loading />;
+
+  if (error) {
+    throw new Error(error?.response?.data?.message);
+  }
+
+  if (!proposals.length) return <h3>یافت نشد</h3>;
+
+  return (
+    <>
+      <h1 className="font-black text-secondary-700 text-xl">درخواست‌ها</h1>
+      <Table>
+        <Table.Header>
+          <th>#</th>
+          <th>توضیحات</th>
+          <th>زمان تحویل</th>
+          <th>هزینه</th>
+          <th>وضعیت</th>
+          <th></th>
+        </Table.Header>
+        <Table.Body>
+          {proposals.map((proposal: any, index: number) => (
+            <ProposalRow key={proposal._id} proposal={proposal} index={index} />
+          ))}
+        </Table.Body>
+      </Table>
+    </>
+  );
 }
