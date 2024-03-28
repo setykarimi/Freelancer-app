476236304e98553ca371becd201d52ffe0a630ee
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-03-29 02:19:59 +0330

    Subject: Debug, Description: Edit for return error, Date: 3/29/2024

--- next-folder/src/hooks/proposals/use-proposals.ts
+++ next-folder/src/hooks/proposals/use-proposals.ts
@@ -2,12 +2,13 @@ import { getProposalsApi } from "@/services/proposal-service";
 import { useQuery } from "@tanstack/react-query";
 
 export default function useProposals() {
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ["proposals"],
     queryFn: getProposalsApi,
   });
 
   const { proposals } = data || {};
+  
 
-  return { proposals, isLoading, isError };
+  return { proposals, isLoading, isError, error };
 }
