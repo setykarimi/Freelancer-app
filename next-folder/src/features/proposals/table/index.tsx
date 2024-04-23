894e321d271cb13c5ac41e61f5b81a66af54136f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-04-23 22:36:22 +0330

    Subject: Debug, Description: Pass default value, Date: 4/23/2024

--- next-folder/src/features/proposals/table/index.tsx
+++ next-folder/src/features/proposals/table/index.tsx
@@ -15,23 +15,20 @@ export default function AllProposalsTable() {
   if (!proposals.length) return <h3>یافت نشد</h3>;
 
   return (
-    <>
-      <h1 className="font-black text-secondary-700 text-xl">درخواست‌ها</h1>
-      <Table>
-        <Table.Header>
-          <th>#</th>
-          <th>توضیحات</th>
-          <th>زمان تحویل</th>
-          <th>هزینه</th>
-          <th>وضعیت</th>
-          <th></th>
-        </Table.Header>
-        <Table.Body>
-          {proposals.map((proposal: any, index: number) => (
-            <ProposalRow key={proposal._id} proposal={proposal} index={index} />
-          ))}
-        </Table.Body>
-      </Table>
-    </>
+    <Table>
+      <Table.Header>
+        <th>#</th>
+        <th>توضیحات</th>
+        <th>زمان تحویل</th>
+        <th>هزینه</th>
+        <th>وضعیت</th>
+        <th></th>
+      </Table.Header>
+      <Table.Body>
+        {proposals.map((proposal: any, index: number) => (
+          <ProposalRow key={proposal._id} proposal={proposal} index={index} />
+        ))}
+      </Table.Body>
+    </Table>
   );
 }
