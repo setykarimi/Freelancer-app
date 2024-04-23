ca0f00625319d30c60f8a0eb9ddab9c753ee4375
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-04-23 22:36:35 +0330

    Subject: Debug, Description: Edit style, Date: 4/23/2024

--- next-folder/src/features/proposal/table/header.tsx
+++ next-folder/src/features/proposal/table/header.tsx
@@ -5,7 +5,7 @@ import { HiArrowRight } from 'react-icons/hi'
 export default function ProposalTableHeader() {
     const moveBack = useMoveBack();
   return (
-    <div className="flex items-center gap-x-4 mb-8">
+    <div className="flex items-center gap-x-4">
     <button onClick={moveBack}>
       <HiArrowRight className="w-5 h-5 text-secondary-500" />
     </button>
