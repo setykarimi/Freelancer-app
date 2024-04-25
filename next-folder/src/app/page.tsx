746ce6af8a0b7d833c22662304feb53e72960251
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-04-26 01:40:30 +0330

    Subject: Main page, Description: Add Landing navbar, Date: 4/26/2024

--- next-folder/src/app/page.tsx
+++ next-folder/src/app/page.tsx
@@ -1,7 +1,14 @@
+import LandingNavbar from "@/common/layout/landing-navbar";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      main page
-    </main>
+    <div className="bg-white w-full">
+      <div className="max-w-[1200px] mx-auto w-full xl:px-0 px-4">
+        <LandingNavbar />
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          main page
+        </main>
+      </div>
+    </div>
   );
 }
