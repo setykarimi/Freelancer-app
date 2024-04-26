4b7e3b4398e22973f942368c3aeb74f3fe1d1200
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-04-26 21:07:10 +0330

    Subject: Main page, Description: Add home page components, Date: 4/26/2024

--- next-folder/src/app/page.tsx
+++ next-folder/src/app/page.tsx
@@ -1,12 +1,17 @@
-import LandingNavbar from "@/common/layout/landing-navbar";
+import CenterSection from "@/features/home/center-section";
+import LandingNavbar from "@/features/home/layout/landing-navbar";
 
 export default function Home() {
   return (
     <div className="bg-white w-full">
       <div className="max-w-[1200px] mx-auto w-full xl:px-0 px-4">
         <LandingNavbar />
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          main page
+        <main className="min-h-screen">
+          <div className="grid grid-cols-3 gap-4 mt-4 w-full">
+            <div></div>
+            <CenterSection />
+            <div></div>
+          </div>
         </main>
       </div>
     </div>
