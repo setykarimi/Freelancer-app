681c326ba64b3860a264c28b358445bcf12b8d55
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-04-27 21:29:59 +0330

    Subject: Main page, Description: Edit style, Date: 4/27/2024

--- next-folder/src/app/page.tsx
+++ next-folder/src/app/page.tsx
@@ -3,14 +3,14 @@ import LandingNavbar from "@/features/home/layout/landing-navbar";
 
 export default function Home() {
   return (
-    <div className="bg-white w-full">
+    <div className="bg-white w-full min-h-screen">
       <div className="max-w-[1200px] mx-auto w-full xl:px-0 px-4">
         <LandingNavbar />
-        <main className="min-h-screen">
-          <div className="grid grid-cols-3 gap-4 mt-4 w-full">
-            <div></div>
+        <main className="max-h-[90vh]">
+          <div className="grid lg:grid-cols-3 gap-4 mt-4 w-full">
+            <div className="lg:block hidden"></div>
             <CenterSection />
-            <div></div>
+            <div className="lg:block hidden"></div>
           </div>
         </main>
       </div>
