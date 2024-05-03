a3e3cc5d3edeeb19bc06bd35b45e682632dc5b4a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-05-03 12:01:39 +0330

    Subject: Main page, Description: Edit style, Date: 5/3/2024

--- next-folder/src/app/page.tsx
+++ next-folder/src/app/page.tsx
@@ -1,5 +1,6 @@
 import CenterSection from "@/features/home/center-section";
 import LandingNavbar from "@/features/home/layout/landing-navbar";
+import LeftSection from "@/features/home/left-section";
 import RightSection from "@/features/home/right-section";
 
 export default function Home() {
@@ -7,11 +8,11 @@ export default function Home() {
     <div className="bg-white w-full min-h-screen">
       <div className="max-w-[1200px] mx-auto w-full xl:px-0 px-4">
         <LandingNavbar />
-        <main className="max-h-[90vh]">
-          <div className="grid lg:grid-cols-3 gap-8 mt-4 w-full">
+        <main className="main-height bg-white">
+          <div className="grid lg:grid-cols-3 gap-8 lg:mt-4 mt-2 w-full">
             <RightSection />
             <CenterSection />
-            <div className="lg:block hidden"></div>
+            <LeftSection />
           </div>
         </main>
       </div>
