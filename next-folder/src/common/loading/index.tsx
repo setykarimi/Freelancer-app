6c2576932bc6beb1ea829375d955ab6f1d369455
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-05-07 23:41:42 +0330

    Subject: Unit test, Description: Add data-test-Id, Date: 5/7/2024

--- next-folder/src/common/loading/index.tsx
+++ next-folder/src/common/loading/index.tsx
@@ -1,21 +1,20 @@
 import { ThreeDots } from "react-loader-spinner";
 import { LoadingPropsType } from "./type";
 
-export default function Loading({
-  width = 76,
-  height = 40,
-}: LoadingPropsType) {
+export default function Loading({ width = 76, height = 40 }: LoadingPropsType) {
   return (
-    <ThreeDots
-      height={height}
-      width={width}
-      radius={9}
-      color="rgb(var(--color-primary-900))"
-      wrapperStyle={{
-        display: "flex",
-        justifyContent: "center",
-      }}
-      visible={true}
-    />
+    <div data-testid="loading-component">
+      <ThreeDots
+        height={height}
+        width={width}
+        radius={9}
+        color="rgb(var(--color-primary-900))"
+        wrapperStyle={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+        visible={true}
+      />
+    </div>
   );
 }
